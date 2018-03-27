6cba801716d65f83e3d630955acd84952e0cf7ea
arash <akbarinia.arash@gmail.com>
2018-03-27 17:29:20 +0200

    cleaning up generate fen bash

--- bash/chess/GenerateFen.sh
+++ bash/chess/GenerateFen.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-# example of using arguments to a script
-#echo "My first name is $1"
-#echo "My surname is $2"
-#echo "My surname is $3"
-#echo "Total number of arguments is $#" 
-
-#-Tr1-0
-#-Tr0-1
-#-Tr1/2
+# convert a pgn file to fen for last position and one move before
 
 echo "Generating fen files!"
 
-indir="/home/arash/Software/repositories/chesscnn/data/pgn/splittedgames/"
-outdir="/home/arash/Software/repositories/chesscnn/data/fen/"
-
-if [ $1 = "white" ]; then
-  outdir=$outdir"white/"
-  indir=$indir"white/"
-elif [ $1 = "black" ]; then
-  outdir=$outdir"black/"
-  indir=$indir"black/"
-else
-  outdir=$outdir"draw/"
-  indir=$indir"draw/"
-fi
+indir=$1
+outdir=$2
 
 echo "Input directory $indir"
 echo "Output directory $outdir"
 
-files=$indir"*.pgn"
+files=$indir*.pgn
 for f in $files; 
 do 
   echo "Processing $f ...";
