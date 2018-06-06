de8909752a9d7be1beb755c278f9fe5b181dd5ac
arash <akbarinia.arash@gmail.com>
2018-06-06 16:13:31 +0200

    computing the histograms of neuron activatins in an indentical manner for all contrast levels

--- matlab/src/dl/kernelanalysis/ActivationDifferentContrasts.m
+++ matlab/src/dl/kernelanalysis/ActivationDifferentContrasts.m
@@ -31,15 +31,19 @@ for contrast = ContrastLevels
 end
 
 nLayers = numel(layers);
+binranges = cell(nLayers, 1);
 % computing the histograms in the range of -300:30:300
-for i = 1:nContrasts
+for i = nContrasts:-1:1
   contrast = ContrastLevels(i);
   ContrastName = sprintf('c%.3u', contrast);
   for l = 1:nLayers
     layer = layers(l);
     LayerName = sprintf('l%.2u', layer);
     features = ActivationReport.cls.(ContrastName).(LayerName).features;
-    histvals = histc(features, -300:30:300, 3);
+    if i == nContrasts
+      binranges{l} = linspace(0.75 * min(features(:)), 0.75 * max(features(:)), 100);
+    end
+    histvals = histc(features, binranges{l}, 3);
     ActivationReport.cls.(ContrastName).(LayerName).histogram = histvals;
   end
 end
