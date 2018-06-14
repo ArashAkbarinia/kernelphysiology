9d55580887fb22c7ded20f723de8444a265f75eb
arash <akbarinia.arash@gmail.com>
2018-06-14 09:33:54 +0200

    first draft of init files

---
+++ python/src/kernelphysiology.py
@@ -0,0 +1,9 @@
+'''Handling the global variables for the project.
+
+'''
+
+import os
+
+python_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(pytho_npath, 'data')
+src_path = os.path.join(python_path, 'src')
