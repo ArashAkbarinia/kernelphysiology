222edc0c0e26036acf20e27819aa32e32e99f15c
arash <akbarinia.arash@gmail.com>
2018-07-03 16:56:23 +0200

    reading the folder of stl results

--- python/src/dl/training/keras/stl/stl_eval_contrast.py
+++ python/src/dl/training/keras/stl/stl_eval_contrast.py
@@ -6,6 +6,7 @@ Testing a Keras model of CIFAR against different levels of contrast.
 import os
 import sys
 import numpy as np
+import glob
 
 import commons
 import stl10
@@ -17,6 +18,11 @@ from utils.imutils import adjust_contrast
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+    
+    if os.path.isdir(args[0]):
+        dirname = args[0]
+        args = sorted(glob.glob(dirname + '*.h5'))
+    
     num_classes = 10
 
     # The data, split between train and test sets:
@@ -30,9 +36,10 @@ if __name__ == "__main__":
     contrasts = np.array([1, 3, 5, 10, 15, 20, 30, 40, 50, 65, 80, 100]) / 100
     
     results = np.zeros((np.size(args, 0), np.size(contrasts, 0)))
-    # loading the model
     i = 0
     for model_path in args:
+        print(model_path)
+        # loading the model
         model = keras.models.load_model(model_path)
         j = 0
         for contrast in contrasts:
