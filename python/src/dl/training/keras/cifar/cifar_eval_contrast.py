3e6108883ceccac564f2238b6c3ea1b3b0b5a109
arash <akbarinia.arash@gmail.com>
2018-07-03 16:56:00 +0200

    cifar contrast experiment

---
+++ python/src/dl/training/keras/cifar/cifar_eval_contrast.py
@@ -0,0 +1,54 @@
+'''
+Testing a Keras model of CIFAR against different levels of contrast.
+'''
+
+
+import os
+import sys
+import numpy as np
+import glob
+
+import commons
+import cifar
+import cifar10
+import cifar100
+
+import keras
+
+from utils.imutils import adjust_contrast
+
+
+if __name__ == "__main__":
+    args = sys.argv[2:]
+    
+    if os.path.isdir(args[0]):
+        dirname = args[0]
+        args = sorted(glob.glob(dirname + '*.h5'))
+    
+    num_classes = int(sys.argv[1])
+    if num_classes == 10:
+        # The data, split between train and test sets:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data(os.path.join(commons.python_root, 'data/datasets/cifar/cifar10/'))
+    elif num_classes == 100:
+        # The data, split between train and test sets:
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine', os.path.join(commons.python_root, 'data/datasets/cifar/cifar100/'))
+    
+    x_test = cifar.preprocess_input(x_test)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    
+    contrasts = np.array([1, 3, 5, 10, 15, 20, 30, 40, 50, 65, 80, 100]) / 100
+    
+    results = np.zeros((np.size(args, 0), np.size(contrasts, 0)))
+    i = 0
+    for model_path in args:
+        print(model_path)
+        # loading the model
+        model = keras.models.load_model(model_path)
+        j = 0
+        for contrast in contrasts:
+            # Score trained model.
+            scores = model.evaluate(adjust_contrast(x_test, contrast), y_test, verbose=1)
+            results[i, j] = scores[1]
+            j += 1
+        i += 1
+    print(results)
