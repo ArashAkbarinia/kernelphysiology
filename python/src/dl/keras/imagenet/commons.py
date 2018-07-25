026b8dd2e94666812cabbd4ffc2aef8414ef9f89
arash <akbarinia.arash@gmail.com>
2018-07-25 12:28:32 +0200

    fixed the bug with path of the project

--- python/src/dl/keras/imagenet/commons.py
+++ python/src/dl/keras/imagenet/commons.py
@@ -12,5 +12,6 @@ current_path = os.getcwd()
 python_root = 'kernelphysiology/python/'
 project_dir = current_path.split(python_root, 1)[0]
 python_root = os.path.join(project_dir, python_root)
+project_dir = os.path.join(project_dir, 'kernelphysiology')
 sys.path += [os.path.join(python_root, 'src/')]
 sys.path += ['/home/arash/Software/repositories/tensorflow/models/research/slim/']
