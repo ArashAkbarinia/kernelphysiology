caaf2f967866f0390d26cafe38dc65fe51c8802c
arash <akbarinia.arash@gmail.com>
2018-07-26 17:57:25 +0200

    merging cifar and stl contrast test

--- python/src/kernelphysiology/dl/keras/cnet_eval_contrast.py
+++ python/src/kernelphysiology/dl/keras/cnet_eval_contrast.py
@@ -1,5 +1,5 @@
 '''
-Testing a Keras model of CIFAR against different levels of contrast.
+Testing a Keras model of CIFAR or STL against different levels of contrast.
 '''
 
 
@@ -9,9 +9,10 @@ import numpy as np
 import glob
 
 import commons
-from kernelphysiology.dl.keras.cifar import cifar_utils
 from kernelphysiology.dl.keras.cifar import cifar10
 from kernelphysiology.dl.keras.cifar import cifar100
+from kernelphysiology.dl.keras.stl import stl10
+from kernelphysiology.dl.keras import contrast_net
 
 import keras
 
@@ -20,24 +21,23 @@ from kernelphysiology.utils.imutils import adjust_contrast
 
 if __name__ == "__main__":
     args = sys.argv[2:]
-    
+
     if os.path.isdir(args[0]):
         dirname = args[0]
         args = sorted(glob.glob(dirname + '*.h5'))
-    
-    num_classes = int(sys.argv[1])
-    if num_classes == 10:
-        # The data, split between train and test sets:
+
+    dataset = sys.argv[1]
+    if dataset.lower() == 'cifar10':
         (x_train, y_train), (x_test, y_test) = cifar10.load_data(os.path.join(commons.python_root, 'data/datasets/cifar/cifar10/'))
-    elif num_classes == 100:
-        # The data, split between train and test sets:
+    elif dataset.lower() == 'cifar100':
         (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine', os.path.join(commons.python_root, 'data/datasets/cifar/cifar100/'))
-    
-    x_test = cifar_utils.preprocess_input(x_test)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-    
+    elif dataset.lower() == 'stl10':
+        (x_train, y_train), (x_test, y_test) = stl10.load_data(os.path.join(commons.python_root, 'data/datasets/stl/stl10/'))
+
+    x_test = contrast_net.preprocess_input(x_test)
+
     contrasts = np.array([1, 3, 5, 10, 15, 20, 30, 40, 50, 65, 80, 100]) / 100
-    
+
     results = np.zeros((np.size(args, 0), np.size(contrasts, 0)))
     i = 0
     for model_path in args:
@@ -51,4 +51,4 @@ if __name__ == "__main__":
             results[i, j] = scores[1]
             j += 1
         i += 1
-    print(results)
+    print(*results)
