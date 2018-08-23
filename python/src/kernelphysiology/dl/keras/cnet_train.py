a49d61718b3ddcc97d8666695a628350af680d1c
arash <akbarinia.arash@gmail.com>
2018-08-23 11:27:58 +0200

    added an argument for checkpoint path

--- python/src/kernelphysiology/dl/keras/cnet_train.py
+++ python/src/kernelphysiology/dl/keras/cnet_train.py
@@ -23,12 +23,11 @@ from kernelphysiology.utils.imutils import adjust_contrast
 
 
 def start_training(args):
+    print('Processing with %d layers in area 1' % args.area1_nlayers)
     print('x_train shape:', args.x_train.shape)
     print(args.x_train.shape[0], 'train samples')
     print(args.x_test.shape[0], 'test samples')
 
-    print('Processing with %d layers in area 1' % args.area1_nlayers)
-    args.model_name += str(args.area1_nlayers)
     args.log_dir = os.path.join(args.save_dir, args.model_name)
     if not os.path.isdir(args.log_dir):
         os.mkdir(args.log_dir)
@@ -39,8 +38,11 @@ def start_training(args):
         args.callbacks.append(check_points)
 
     args.area1_nlayers = int(args.area1_nlayers)
-    
-    model = cnet.build_classifier_model(confs=args)
+
+    if args.checkpoint_path == None:
+        model = cnet.build_classifier_model(confs=args)
+    else:
+        model = keras.models.load_model(args.checkpoint_path)
 
     initial_lr = 1e-3
     def lr_scheduler(epoch):
@@ -107,7 +109,8 @@ if __name__ == "__main__":
     parser.add_argument('--a1dilation', dest='area1_dilation', action='store_true', default=False, help='Whether to include dilation in kernels in area 1 (default: False)')
     parser.add_argument('--dog', dest='add_dog', action='store_true', default=False, help='Whether to add a DoG layer (default: False)')
     parser.add_argument('--mg', dest='multi_gpus', type=int, default=None, help='The number of GPUs to be used (default: None)')
-    parser.add_argument('--name', dest='experiment_name', type=str, default='', help='The name of the experiment (default: None)')
+    parser.add_argument('--name', dest='experiment_name', type=str, default='Ex', help='The name of the experiment (default: Ex)')
+    parser.add_argument('--checkpoint_path', dest='checkpoint_path', type=str, default=None, help='The path to a previous checkpoint to continue (default: None)')
     parser.add_argument('--batch_size', dest='batch_size', type=int, default=32, help='Batch size (default: 64)')
     parser.add_argument('--epochs', dest='epochs', type=int, default=50, help='Number of epochs (default: 50)')
     parser.add_argument('--log_period', dest='log_period', type=int, default=0, help='The period of logging the network (default: 0)')
@@ -115,13 +118,13 @@ if __name__ == "__main__":
     parser.add_argument('--data_augmentation', dest='data_augmentation', action='store_true', default=False, help='Whether to augment data (default: False)')
 
     args = parser.parse_args()
-    which_model = args.dataset.lower()
+    dataset_name = args.dataset.lower()
     # preparing arguments
-    args.save_dir = os.path.join(commons.python_root, 'data/nets/%s/%s/' % (''.join([i for i in which_model if not i.isdigit()]), which_model))
+    args.save_dir = os.path.join(commons.python_root, 'data/nets/%s/%s/' % (''.join([i for i in dataset_name if not i.isdigit()]), dataset_name))
     args.dog_path = os.path.join(args.save_dir, 'dog.h5')
 
     # preparing the name of the model
-    args.model_name = 'keras_%s_area_%s_%s_' % (which_model, args.experiment_name, args.train_contrast)
+    args.model_name = 'keras_%s_area_%d_%s_contrast_%d_' % (dataset_name, args.area1_nlayers, args.experiment_name, args.train_contrast)
     if args.area1_batchnormalise:
         args.model_name += 'bnr_'
     if args.area1_activation:
@@ -134,11 +137,11 @@ if __name__ == "__main__":
         args.model_name += 'dog_'
 
     # which model to run
-    if which_model == 'cifar10':
+    if dataset_name == 'cifar10':
         args = cifar_train.prepare_cifar10(args)
-    elif which_model == 'cifar100':
+    elif dataset_name == 'cifar100':
         args = cifar_train.prepare_cifar100(args)
-    elif which_model == 'stl10':
+    elif dataset_name == 'stl10':
         args = stl_train.prepare_stl10(args)
 
     start_training(args)
