0fc0880165e0562244f0b913642604b9e14735b8
arash <akbarinia.arash@gmail.com>
2018-08-24 16:51:06 +0200

    a function to set one area to not tainable

--- python/src/kernelphysiology/dl/keras/utils.py
+++ python/src/kernelphysiology/dl/keras/utils.py
@@ -13,3 +13,15 @@ def get_conv2ds(model, topn=math.inf):
         if type(model.layers[i]) is keras.layers.convolutional.Conv2D:
             conv2d_inds.append(i)
     return conv2d_inds
+
+
+def set_area_trainable_false(model, num_areas):
+    current_area = 1
+    for i in range(0, len(model.layers)):
+        if type(model.layers[i]) is keras.layers.pooling.MaxPooling2D:
+            if num_areas == current_area:
+                break
+            current_area += 1
+        else:
+            model.layers[i].trainable = False
+    return model
