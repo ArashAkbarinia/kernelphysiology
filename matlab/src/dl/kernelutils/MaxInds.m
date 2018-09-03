1dccb1f98d681ae6306a04b788b3dd28c8f8451e
arash <akbarinia.arash@gmail.com>
2018-09-03 19:15:38 +0200

    finding the maxpooling layers

---
+++ matlab/src/dl/kernelutils/MaxInds.m
@@ -0,0 +1,27 @@
+function MaxLayers = MaxInds(net, topn)
+%MaxInds  Finds the indices of the first max-pooling layers.
+%
+% inputs
+%  net   the network.
+%  topn  the number of layers to be returned, by default all.
+%
+% outputs
+%  MaxLayers  indices of the first N max pooling layers.
+%
+
+if nargin < 2
+  topn = inf;
+end
+
+MaxLayers = [];
+for i = 1:numel(net.Layers)
+  if isa(net.Layers(i), 'nnet.cnn.layer.MaxPooling2DLayer')
+    MaxLayers(end + 1) = i; %#ok
+    if numel(MaxLayers) == topn
+      return;
+    end
+  end
+end
+
+end
+
