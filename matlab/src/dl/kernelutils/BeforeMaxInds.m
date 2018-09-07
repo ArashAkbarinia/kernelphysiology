27495d7c2f3e1c632c8a4b82538dfef33fe47909
arash <akbarinia.arash@gmail.com>
2018-09-07 18:27:01 +0200

    finding the layer before max pooling

---
+++ matlab/src/dl/kernelutils/BeforeMaxInds.m
@@ -0,0 +1,35 @@
+function BeforeMaxLayers = BeforeMaxInds(net, topn)
+%BeforeMaxInds  Finds the indices of the layers before max-pooling.
+%
+% inputs
+%  net   the network.
+%  topn  the number of layers to be returned, by default all.
+%
+% outputs
+%  BeforeMaxLayers  indices of the layers before each max pooling.
+%
+
+if nargin < 2
+  topn = inf;
+end
+
+BeforeMaxLayers = [];
+for i = 1:numel(net.Layers)
+  if isa(net.Layers(i), 'nnet.cnn.layer.MaxPooling2DLayer')
+    if isa(net, 'DAGNetwork')
+      ConnectionId = strcmpi(net.Connections.Destination, net.Layers(i).Name);
+      LayerId = LayerName2Id(net, net.Connections.Source(ConnectionId));
+      BeforeMaxLayers(end + 1) = LayerId; %#ok
+    elseif isa(net, 'SeriesNetwork')
+      BeforeMaxLayers(end + 1) = i - 1; %#ok
+    else
+      error('Unsopported network architecture!');
+    end
+    
+    if numel(BeforeMaxLayers) == topn
+      return;
+    end
+  end
+end
+
+end
