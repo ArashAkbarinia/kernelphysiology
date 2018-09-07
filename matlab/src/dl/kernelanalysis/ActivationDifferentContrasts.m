f6ee1154243ceb6d5d8e7776ba8ae67ebc2f089c
arash <akbarinia.arash@gmail.com>
2018-09-07 16:10:09 +0200

    computing the histogram comparison among kernels (2nd) dimension

--- matlab/src/dl/kernelanalysis/ActivationDifferentContrasts.m
+++ matlab/src/dl/kernelanalysis/ActivationDifferentContrasts.m
@@ -49,14 +49,29 @@ for i = nContrasts:-1:1
       % the rane.
       binranges{l} = [-inf, binranges{l}, +inf];
     end
+    % computing the histograms along the pixel dimention (3rd)
     histvals = histc(features, binranges{l}, 3);
     histvals = histvals ./ sum(histvals, 3);
-    ActivationReport.cls.(ContrastName).(LayerName).histogram = histvals;
+    ActivationReport.cls.(ContrastName).(LayerName).histogram3 = histvals;
+    
+    % computing the histograms along the kernel dimention (2nd)
+    [~, ~, chnsk] = size(features);
+    KernelHistograms = zeros(chnsk, nEdges);
+    for k = 1:chnsk
+      CurrentKernel = features(:, :, k);
+      % when computing the histogram for the kernel we don't consider the
+      % previous ranges, we basically are interested to check whether the
+      % shape remains the same
+      histvals = histcounts(CurrentKernel(:), nEdges);
+      KernelHistograms(k, :) = histvals ./ sum(histvals);
+    end
+    ActivationReport.cls.(ContrastName).(LayerName).histogram2 = KernelHistograms;
   end
 end
 
 ActivationReport.CompMatrix = zeros(nContrasts, nContrasts, nLayers);
 ActivationReport.CompMatrixHist = zeros(nContrasts, nContrasts, nLayers);
+ActivationReport.kernels.CompMatrixHist = zeros(nContrasts, nContrasts, nLayers);
 for i = 1:nContrasts
   contrast1 = ContrastLevels(i);
   ContrastName1 = sprintf('c%.3u', contrast1);
@@ -66,20 +81,32 @@ for i = 1:nContrasts
     for l = 1:nLayers
       layer = layers(l);
       LayerName = sprintf('l%.2u', layer);
+      
+      % most activated kernel along pixel dumension (3rd)
       activity1 = ActivationReport.cls.(ContrastName1).(LayerName).top{2};
       activity2 = ActivationReport.cls.(ContrastName2).(LayerName).top{2};
+      
+      % all those pixels whose kernels remained identical
       DiffActivity = activity1 - activity2;
       PerIdenticalNeurons = sum(DiffActivity(:) == 0) / numel(DiffActivity(:));
       ActivationReport.CompMatrix(i, j, l) = PerIdenticalNeurons;
       
-      % histogram comparisons
-      hist1 = ActivationReport.cls.(ContrastName1).(LayerName).histogram;
-      hist2 = ActivationReport.cls.(ContrastName2).(LayerName).histogram;
+      % histogram comparisons along pixel dimension (3rd)
+      HistPixs1 = ActivationReport.cls.(ContrastName1).(LayerName).histogram3;
+      HistPixs2 = ActivationReport.cls.(ContrastName2).(LayerName).histogram3;
       
       % Euclidean distance between histograms
-      HistDiff = (hist1 - hist2) .^ 2;
-      HistDiff = sqrt(sum(HistDiff, 3));
-      ActivationReport.CompMatrixHist(i, j, l) = mean(HistDiff(:));
+      DiffHistPixs = (HistPixs1 - HistPixs2) .^ 2;
+      DiffHistPixs = sqrt(sum(DiffHistPixs, 3));
+      ActivationReport.CompMatrixHist(i, j, l) = mean(DiffHistPixs(:));
+      
+      % histogram comparisons along the kernel dimension (2nd)
+      HistKers1 = ActivationReport.cls.(ContrastName1).(LayerName).histogram2;
+      HistKers2 = ActivationReport.cls.(ContrastName2).(LayerName).histogram2;
+      
+      DiffHistKers = (HistKers1 - HistKers2) .^ 2;
+      DiffHistKers = sqrt(sum(DiffHistKers, 2));
+      ActivationReport.kernels.CompMatrixHist(i, j, l) = mean(DiffHistKers(:));
     end
   end
 end
@@ -112,6 +139,7 @@ for layer = layers
   li = net.Layers(layer);
   
   features = activations(net, inim, net.Layers(layer).Name);
+  % finding the most activated kernel along the pixel dimention (3rd)
   [fsorted, finds] = sort(features, 3, 'descend');
   
   LayerReport.features = features;
@@ -122,13 +150,17 @@ for layer = layers
   
   if SaveImages
     nkernels = size(features, 3);
+    % colour coding each kernel to one colour
     cmap = DistinguishableColours(nkernels);
     
     rgbim = label2rgb(LayerReport.top{2}, cmap);
     imwrite(rgbim, sprintf('%s%s-l%.2u.png', outdir, prefix, layer));
     
+    % saving all the kernels actitivites in a montage format
     h = figure('visible', 'off');
-    montage(features);
+    minval = min(min(features));
+    maxval = max(max(features));
+    montage(NormaliseChannel(features, 0, 1, minval, maxval));
     title(['Layer ', li.Name, ' Activities']);
     saveas(h, sprintf('%s%s-montage%.2u.png', outdir, prefix, layer));
     close(h);
