9ab8c9a8ccf34f3c272c036bbdc568070abc2756
arash <akbarinia.arash@gmail.com>
2018-09-07 18:43:27 +0200

    minor commit

--- matlab/src/psychophysical/scripts/ExperimentAllPretrained.m
+++ matlab/src/psychophysical/scripts/ExperimentAllPretrained.m
@@ -3,14 +3,15 @@ run /home/arash/Software/repositories/kernelphysiology/matlab/src/kernelphysiolo
 NetNames = {'vgg16', 'vgg19', 'alexnet', 'googlenet', 'inceptionv3', 'resnet50', 'resnet101', 'vgg3c4x'};
 
 DatasetName = 'ilsvrc-test';
-AnalysisDir = '/home/deeplearning/Desktop/maxresults/';
+AnalysisDir = '/home/deeplearning/Desktop/arash/resize/maxresults/';
 % AnalysisDir = '/home/arash/Software/repositories/kernelphysiology/analysis/kernelsactivity/';
 % DatasetName = 'ilsvrc2017';
 
+LayerType = 'max';
 for i = 1:numel(NetNames)
   NetwrokName = NetNames{i};
   outdir = [AnalysisDir, NetwrokName, '/'];
   mkdir(outdir);
   fprintf('Processing network %s\n', NetwrokName);
-  DatasetActivationDifferentContrasts(NetwrokName, DatasetName, outdir);
+  DatasetActivationDifferentContrasts(NetwrokName, DatasetName, outdir, LayerType);
 end
