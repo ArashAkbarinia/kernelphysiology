cde5bbdf7a66d727596cf48a429d42dac219f011
arash <akbarinia.arash@gmail.com>
2018-09-14 10:55:33 +0200

    computing the percentage change at 3rd dimension

---
+++ matlab/src/utils/mathelpers/PercentageChange3.m
@@ -0,0 +1,26 @@
+function pchange = PercentageChange3(mat1, mat2, TopPercentile)
+%PercentageChange3  Computes the percentage of changes betwenn two matrices
+%
+% inputs
+%   mat1  the input matrix 1
+%   mat2  the input matrix 2
+%   TopPercentile  which percentile to eb considered for computation
+%
+% outputs
+%   pchange  percentage of change at every pixel.
+%
+
+pchange = abs(mat1 - mat2) ./ max(abs(mat1), abs(mat2));
+
+if nargin > 2
+  [~, ~, chnsk] = size(mat1);
+  KernelTopPercentile = ceil(double(chnsk) .* TopPercentile);
+  kinds = 1:KernelTopPercentile;
+  
+  pchange = sort(pchange, 3, 'descend');
+  pchange = pchange(:, :, kinds);
+end
+
+pchange = mean(pchange, 3);
+
+end
