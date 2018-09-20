15d104d98d2ae0654e5f2518f22060d42799287f
arash <akbarinia.arash@gmail.com>
2018-09-20 11:33:16 +0200

    added flag to use certain GPUs in awesome

--- python/src/kernelphysiology/dl/keras/imagenet/commons.py
+++ python/src/kernelphysiology/dl/keras/imagenet/commons.py
@@ -5,6 +5,7 @@ The common files used in imagenet.
 
 import os
 import sys
+import socket
 
 
 # finding the root of the project
@@ -14,4 +15,7 @@ project_dir = current_path.split(python_root, 1)[0]
 python_root = os.path.join(project_dir, python_root)
 project_dir = os.path.join(project_dir, 'kernelphysiology')
 sys.path += [os.path.join(python_root, 'src/')]
-sys.path += ['/home/arash/Software/repositories/tensorflow/models/research/slim/']
+
+if socket.gethostname() == 'awesome':
+    # list of GPUs allocated to me
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
