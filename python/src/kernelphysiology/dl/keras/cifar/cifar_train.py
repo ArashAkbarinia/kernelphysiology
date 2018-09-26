88359f4805c75055a7c289aabfbc94d4f3dd7f99
arash <akbarinia.arash@gmail.com>
2018-09-26 11:57:22 +0200

    aded resize generator for cifar

--- python/src/kernelphysiology/dl/keras/cifar/cifar_train.py
+++ python/src/kernelphysiology/dl/keras/cifar/cifar_train.py
@@ -6,6 +6,8 @@ Train a simple DNN on CIFAR 10 or 100.
 from kernelphysiology.dl.keras.cifar import cifar10
 from kernelphysiology.dl.keras.cifar import cifar100
 
+from kernelphysiology.dl.keras.utils import ResizeGenerator
+
 
 def prepare_cifar10(args):
     args.num_classes = 10
@@ -16,10 +18,38 @@ def prepare_cifar10(args):
     return args
 
 
+def prepare_cifar10_generators(args):
+    args.num_classes = 10
+
+    # The data, split between train and test sets:
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    args = get_generators(args, x_train, y_train, x_test, y_test)
+    return args
+
+
 def prepare_cifar100(args):
     args.num_classes = 100
 
     # The data, split between train and test sets:
     (args.x_train, args.y_train), (args.x_test, args.y_test) = cifar100.load_data()
+
+    return args
+
+
+def prepare_cifar100_generators(args):
+    args.num_classes = 100
+
+    # The data, split between train and test sets:
+    (x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+    args = get_generators(args, x_train, y_train, x_test, y_test)
+    return args
+
+
+def get_generators(args, x_train, y_train, x_test, y_test):
+    args.train_generator = ResizeGenerator(x_train, y_train, args.num_classes, batch_size=args.batch_size, target_size=args.target_size, preprocessing_function=args.preprocessing_function)
+
+    args.validation_generator = ResizeGenerator(x_test, y_test, args.num_classes, batch_size=args.batch_size, target_size=args.target_size, preprocessing_function=args.preprocessing_function)
 
     return args
