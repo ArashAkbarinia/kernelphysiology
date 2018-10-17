905e22f0bc3af7ee8aed3af3f8795a082bf33d80
arash <akbarinia.arash@gmail.com>
2018-10-17 12:09:24 +0200

    option of random local variation for adjusting contrast

--- matlab/src/psychophysical/visual/AdjustContrast.m
+++ matlab/src/psychophysical/visual/AdjustContrast.m
@@ -1,12 +1,22 @@
-function OutImage = AdjustContrast(InputImage, ContrastLevel)
+function OutImage = AdjustContrast(InputImage, ContrastLevel, PixelDeviation)
 %AdjustContrast  it scales the image to a certain contrast level in [0, 1].
 %   https://github.com/rgeirhos/object-recognition/blob/master/code/image-manipulation.py
 % contrast_level: a scalar in [0, 1]; with 1 -> full contrast
+
+if nargin < 3
+  PixelDeviation = 0;
+end
 
 InputImage = im2double(InputImage);
 
 assert(ContrastLevel >= 0.0, 'ContrastLevel too low.');
 assert(ContrastLevel <= 1.0, 'ContrastLevel too high.');
+
+MinContrast = ContrastLevel - PixelDeviation;
+MaxContrast = ContrastLevel + PixelDeviation;
+
+PixelDeviation = (MaxContrast - MinContrast) .* rand(size(InputImage)) + MinContrast;
+ContrastLevel = ones(size(InputImage)) .* (ContrastLevel + PixelDeviation);
 
 OutImage = (1 - ContrastLevel) ./ 2.0 + InputImage .* ContrastLevel;
 
