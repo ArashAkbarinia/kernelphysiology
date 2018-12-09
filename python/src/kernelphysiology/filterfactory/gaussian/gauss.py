a02c0215bc3aa23e91d1a58a7e031f5924b487cf
arash <akbarinia.arash@gmail.com>
2018-12-09 10:40:10 +0100

    gaussian 2d filter

--- python/src/kernelphysiology/filterfactory/gaussian/gauss.py
+++ python/src/kernelphysiology/filterfactory/gaussian/gauss.py
@@ -1,12 +1,46 @@
 import numpy as np
-import scipy.stats as st
+import math
 
-def gkern(kernlen=21, nsig=3):
-    """Returns a 2D Gaussian kernel array."""
 
-    interval = (2*nsig+1.)/(kernlen)
-    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-    kernel = kernel_raw/kernel_raw.sum()
+def gaussian_width(sigma, max_width=100, threshold=1e-4):
+    filter_width = 1
+    for pw in range(np.floor(max_width / 2).astype('int'), -1, -1):
+        if np.exp(-(pw ** 2) / (2 * sigma ** 2)) > threshold:
+            filter_width = pw
+            break
+    return filter_width * 2 + 1
+
+
+def gaussian_kernel(sigmax, sigmay=None, meanx=0, meany=0, theta=0, width=None, threshold=1e-4):
+    if sigmax == 0:
+        return 1
+    if sigmay is None:
+        sigmay = sigmax
+        max_sigma = sigmax
+    else:
+        max_sigma = np.maximum(sigmax, sigmay)
+    if width is None:
+        sizex = gaussian_width(sigma=max_sigma, max_width=100, threshold=threshold)
+    else:
+        sizex = width
+    sizey = sizex
+ 
+    centrex = (sizex + 1) / 2
+    centrey = (sizey + 1) / 2
+    centrex = centrex + (meanx * centrex)
+    centrey = centrey + (meany * centrey)
+
+    a =  math.cos(theta) ** 2 / 2 / sigmax ** 2 + math.sin(theta) ** 2 / 2 / sigmay ** 2
+    b = -math.sin(2 * theta) / 4 / sigmax ** 2 + math.sin(2 * theta) / 4 / sigmay ** 2
+    c =  math.sin(theta) ** 2 / 2 / sigmax ** 2 + math.cos(theta) ** 2 / 2 / sigmay ** 2
+
+    kernel = np.zeros((sizex, sizey))
+    for i in range(sizex):
+        for j in range(sizey):
+            x = (i + 1) * 1 - centrex
+            y = 1 * (j + 1) - centrey
+            kernel[i, j] = np.exp(-(a * x ** 2 + 2 * b * x * y + c * y ** 2))
+
+    kernel /= kernel.sum()
+
     return kernel
