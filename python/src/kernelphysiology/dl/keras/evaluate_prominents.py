d6829991e35308530ed8fe37dda42151fab43a27
arash <akbarinia.arash@gmail.com>
2018-12-18 17:10:02 +0100

    minor bug with improt dataset

--- python/src/kernelphysiology/dl/keras/evaluate_prominents.py
+++ python/src/kernelphysiology/dl/keras/evaluate_prominents.py
@@ -14,9 +14,9 @@ import tensorflow as tf
 import keras
 from keras.utils import multi_gpu_model
 
-from kernelphysiology.dl.keras.prominent_utils import test_prominent_prepares, test_arg_parser
+from kernelphysiology.dl.keras.prominent_utils import test_prominent_prepares, test_arg_parser, which_dataset
 from kernelphysiology.dl.keras.utils import get_top_k_accuracy
-from kernelphysiology.dl.keras.models.utils import which_network, which_dataset, get_preprocessing_function
+from kernelphysiology.dl.keras.models.utils import which_network, get_preprocessing_function
 from kernelphysiology.utils.imutils import uniform_noise, gaussian_blur, s_p_noise
 from kernelphysiology.utils.imutils import adjust_gamma, adjust_contrast, adjust_illuminant
 
