0168ebd891be004c6979f904d03b577cca769044
arash <akbarinia.arash@gmail.com>
2018-12-20 11:41:52 +0100

    organising the argument parset

--- python/src/kernelphysiology/dl/keras/prominent_utils.py
+++ python/src/kernelphysiology/dl/keras/prominent_utils.py
@@ -4,6 +4,7 @@ Utility functions for training prominent networks.
 
 
 import os
+import sys
 import glob
 import argparse
 import datetime
@@ -185,8 +186,8 @@ def common_arg_parser(description):
     parser.add_argument('--gpus', nargs='+', type=int, default=[0], help='List of GPUs to be used (default: [0])')
     parser.add_argument('--workers', type=int, default=1, help='Number of workers for image generator (default: 1)')
 
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size (default: 32)')
-    parser.add_argument('--target_size', type=int, default=224, help='Target size (default: 224)')
+    parser.add_argument('--batch_size', type=int, default=None, help='Batch size (default: according to dataset)')
+    parser.add_argument('--target_size', type=int, default=None, help='Target size (default: according to dataset)')
     parser.add_argument('--crop_centre', action='store_true', default=False, help='Crop the image to its centre (default: False)')
     parser.add_argument('--preprocessing', type=str, default=None, help='The preprocessing function (default: network preprocessing function)')
     parser.add_argument('--top_k', type=int, default=5, help='Accuracy of top K elements (default: 5)')
@@ -199,7 +200,7 @@ def activation_arg_parser(argvs):
 
     parser.add_argument('--contrasts', nargs='+', type=float, default=[100], help='List of contrasts to be evaluated (default: [100])')
 
-    return check_args(parser, argvs)
+    return check_args(parser, argvs, 'activation')
 
 
 def test_arg_parser(argvs):
@@ -215,64 +216,101 @@ def test_arg_parser(argvs):
     image_degradation_group.add_argument('--gammas', nargs='+', type=float, default=None, help='List of gammas to be evaluated (default: None)')
     image_degradation_group.add_argument('--illuminants', nargs='+', type=float, default=None, help='List of illuminations to be evaluated (default: None)')
 
-    return check_args(parser, argvs)
+    return check_args(parser, argvs, 'testing')
 
 
 def train_arg_parser(argvs):
     parser = common_arg_parser('Training prominent nets of Keras.')
 
     # better handling the parameters, e.g. pretrained ones are only for imagenet
-    parser.add_argument('--area1layers', type=int, default=None, help='The number of layers in area 1 (default: None)')
+    architecture_group = parser.add_argument_group('architecture')
+    architecture_group.add_argument('--area1layers', type=int, default=None, help='The number of layers in area 1 (default: None)')
 
-    weights_group = parser.add_mutually_exclusive_group()
+    initialisation_group = parser.add_argument_group('initialisation')
+    weights_group = initialisation_group.add_mutually_exclusive_group()
     weights_group.add_argument('--load_weights', type=str, default=None, help='Whether loading weights from a model (default: None)')
     weights_group.add_argument('--initialise', type=str, default=None, help='Whether using a specific initialisation of weights (default: None)')
-    parser.add_argument('--tog_sigma', type=float, default=1, help='Sigma of ToG initialisation (default: 1)')
-    parser.add_argument('--tog_surround', type=float, default=5, help='Surround enlargement in ToG initialisation (default: 5)')
-    parser.add_argument('--g_sigmax', type=float, default=1, help='Sigma-x of Gaussian initialisation (default: 1)')
-    parser.add_argument('--g_sigmay', type=float, default=None, help='Sigma-y of Gaussian initialisation (default: None)')
-    parser.add_argument('--g_meanx', type=float, default=0, help='Mean-x of Gaussian initialisation (default: 0)')
-    parser.add_argument('--g_meany', type=float, default=0, help='Mean-y of Gaussian initialisation (default: 0)')
-    parser.add_argument('--g_theta', type=float, default=0, help='Theta of Gaussian initialisation (default: 0)')
-    parser.add_argument('--gg_sigma', type=float, default=1, help='Sigma of Gaussian gradient initialisation (default: 1)')
-    parser.add_argument('--gg_theta', type=float, default=math.pi/2, help='Theta of Gaussian gradient initialisation (default: 1)')
-    parser.add_argument('--gg_seta', type=float, default=1, help='Seta of Gaussian gradient initialisation (default: 1)')
+    initialisation_group.add_argument('--tog_sigma', type=float, default=1, help='Sigma of ToG (default: 1)')
+    initialisation_group.add_argument('--tog_surround', type=float, default=5, help='Surround enlargement in ToG (default: 5)')
+    initialisation_group.add_argument('--g_sigmax', type=float, default=1, help='Sigma-x of Gaussian (default: 1)')
+    initialisation_group.add_argument('--g_sigmay', type=float, default=None, help='Sigma-y of Gaussian (default: None)')
+    initialisation_group.add_argument('--g_meanx', type=float, default=0, help='Mean-x of Gaussian (default: 0)')
+    initialisation_group.add_argument('--g_meany', type=float, default=0, help='Mean-y of Gaussian (default: 0)')
+    initialisation_group.add_argument('--g_theta', type=float, default=0, help='Theta of Gaussian (default: 0)')
+    initialisation_group.add_argument('--gg_sigma', type=float, default=1, help='Sigma of Gaussian gradient (default: 1)')
+    initialisation_group.add_argument('--gg_theta', type=float, default=math.pi/2, help='Theta of Gaussian gradient (default: 1)')
+    initialisation_group.add_argument('--gg_seta', type=float, default=1, help='Seta of Gaussian gradient (default: 1)')
 
-    parser.add_argument('--optimiser', type=str, default='adam', help='The optimiser to be used (default: adam)')
-    parser.add_argument('--lr', type=float, default=None, help='The learning rate parameter of optimiser (default: None)')
-    parser.add_argument('--decay', type=float, default=None, help='The decay weight parameter of optimiser (default: None)')
-    parser.add_argument('--exp_decay', type=float, default=None, help='The exponential decay (default: None)')
-    parser.add_argument('--lr_schedule', type=str, default=None, help='The custom learning rate scheduler (default: None)')
+    optimisation_group = parser.add_argument_group('optimisation')
+    optimisation_group.add_argument('--optimiser', type=str, default='adam', help='The optimiser to be used (default: adam)')
+    optimisation_group.add_argument('--lr', type=float, default=None, help='The learning rate parameter (default: None)')
+    optimisation_group.add_argument('--decay', type=float, default=None, help='The decay weight parameter (default: None)')
+    optimisation_group.add_argument('--exp_decay', type=float, default=None, help='The exponential decay (default: None)')
+    optimisation_group.add_argument('--lr_schedule', type=str, default=None, help='The custom learning rate scheduler (default: None)')
+    optimisation_group.add_argument('--epochs', type=int, default=50, help='Number of epochs (default: 50)')
+    optimisation_group.add_argument('--initial_epoch', type=int, default=0, help='The initial epoch number (default: 0)')
 
-    parser.add_argument('--epochs', type=int, default=50, help='Number of epochs (default: 50)')
-    parser.add_argument('--initial_epoch', type=int, default=0, help='The initial epoch number (default: 0)')
-    parser.add_argument('--log_period', type=int, default=0, help='The period of logging the epochs weights (default: 0)')
-    parser.add_argument('--steps_per_epoch', type=int, default=None, help='Number of steps per epochs (default: number of samples divided by the batch size)')
-    parser.add_argument('--validation_steps', type=int, default=None, help='Number of steps for validations (default: number of samples divided by the batch size)')
+    logging_group = parser.add_argument_group('logging')
+    logging_group.add_argument('--log_period', type=int, default=0, help='The period of logging the epochs weights (default: 0)')
+    logging_group.add_argument('--steps_per_epoch', type=int, default=None, help='Number of steps per epochs (default: number of samples divided by the batch size)')
+    logging_group.add_argument('--validation_steps', type=int, default=None, help='Number of steps for validations (default: number of samples divided by the batch size)')
 
-    parser.add_argument('--noshuffle', dest='shuffle', action='store_false', default=True, help='Whether to stop shuffling data (default: False)')
-    parser.add_argument('--horizontal_flip', action='store_true', default=False, help='Whether to perform horizontal flip data (default: False)')
-    parser.add_argument('--vertical_flip', action='store_true', default=False, help='Whether to perform vertical flip (default: False)')
-    parser.add_argument('--zoom_range', type=float, default=0, help='Value for zoom agumentation (default: 0)')
-    parser.add_argument('--width_shift_range', type=float, default=0, help='Value for width shift agumentation (default: 0)')
-    parser.add_argument('--height_shift_range', type=float, default=0, help='Value for height shift agumentation (default: 0)')
+    keras_augmentation_group = parser.add_argument_group('keras augmentation')
+    keras_augmentation_group.add_argument('--noshuffle', dest='shuffle', action='store_false', default=True, help='Stop shuffling data (default: False)')
+    keras_augmentation_group.add_argument('--horizontal_flip', action='store_true', default=False, help='Perform horizontal flip data (default: False)')
+    keras_augmentation_group.add_argument('--vertical_flip', action='store_true', default=False, help='Perform vertical flip (default: False)')
+    keras_augmentation_group.add_argument('--zoom_range', type=float, default=0, help='Range of zoom agumentation (default: 0)')
+    keras_augmentation_group.add_argument('--width_shift_range', type=float, default=0, help='Range of width shift (default: 0)')
+    keras_augmentation_group.add_argument('--height_shift_range', type=float, default=0, help='Range of height shift (default: 0)')
 
-    parser.add_argument('--contrast_range', type=float, default=None, help='Value to perform contrast agumentation (default: None)')
-    parser.add_argument('--local_contrast_variation', type=float, default=0, help='Value to deviate local contrast augmentation (default: 0)')
-    parser.add_argument('--illuminant_range', type=float, default=None, help='Value to perform illumination agumentation (default: None)')
-    parser.add_argument('--local_illuminant_variation', type=float, default=0, help='Value to deviate local illumination augmentation (default: 0)')
-    parser.add_argument('--gaussian_sigma', type=float, default=None, help='Value to perform Gaussian blurring agumentation (default: None)')
-    parser.add_argument('--s_p_amount', type=float, default=None, help='Value to perform salt&pepper agumentation (default: None)')
-    parser.add_argument('--gaussian_amount', type=float, default=None, help='Value to perform Gaussian noise agumentation (default: None)')
+    our_augmentation_group = parser.add_argument_group('our augmentation')
+    our_augmentation_group.add_argument('--contrast_range', type=float, default=None, help='Contrast lower limit (default: None)')
+    our_augmentation_group.add_argument('--local_contrast_variation', type=float, default=0, help='Contrast local variation (default: 0)')
+    our_augmentation_group.add_argument('--illuminant_range', type=float, default=None, help='Lower illuminant limit (default: None)')
+    our_augmentation_group.add_argument('--local_illuminant_variation', type=float, default=0, help='Illuminant local variation (default: 0)')
+    our_augmentation_group.add_argument('--gaussian_sigma', type=float, default=None, help='Gaussian blurring upper limit (default: None)')
+    our_augmentation_group.add_argument('--s_p_amount', type=float, default=None, help='Salt&pepper upper limit (default: None)')
+    our_augmentation_group.add_argument('--gaussian_amount', type=float, default=None, help='Gaussian noise upper limit (default: None)')
 
-    return check_args(parser, argvs)
+    return check_args(parser, argvs, 'training')
 
 
-def check_args(parser, argvs):
+def check_args(parser, argvs, script_type):
     # NOTE: this is just in order to get rid of EXIF warnigns
     warnings.filterwarnings("ignore", "(Possibly )?corrupt EXIF data", UserWarning)
 
     args = parser.parse_args(argvs)
+
+    args.script_type = script_type
+
+    # setting the target size
+    if args.target_size is None:
+        if args.dataset == 'imagenet':
+            args.target_size = 224
+        elif args.dataset == 'cifar10' or args.dataset == 'cifar100' or args.dataset == 'stl10':
+            args.target_size = 32
+        else:
+            sys.exit('target_size is required for dataset %s' % (args.dataset))
+
+    # setting the batch size
+    if args.batch_size is None:
+        if args.dataset == 'imagenet':
+            if args.script_type == 'training':
+                args.batch_size = 32
+            if args.script_type == 'testing':
+                args.batch_size = 64
+            if args.script_type == 'activation':
+                args.batch_size = 32
+        elif args.dataset == 'cifar10' or args.dataset == 'cifar100' or args.dataset == 'stl10':
+            if args.script_type == 'training':
+                args.batch_size = 256
+            if args.script_type == 'testing':
+                args.batch_size = 512
+            if args.script_type == 'activation':
+                args.batch_size = 256
+        else:
+            sys.exit('batch_size is required for dataset %s' % (args.dataset))
+
     # TODO: more checking for GPUs
     os.environ["CUDA_VISIBLE_DEVICES"] = ', '.join(str(e) for e in args.gpus)
 
