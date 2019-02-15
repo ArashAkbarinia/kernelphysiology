7393ee7a7da116618a732f736f55be9e06f5a69b
Arash <akbarinia.arash@gmail.com>
2019-02-15 11:34:14 +0100

    first draft of coco training

--- python/src/kernelphysiology/dl/keras/datasets/coco/coco.py
+++ python/src/kernelphysiology/dl/keras/datasets/coco/coco.py
@@ -14,6 +14,50 @@ from kernelphysiology.dl.keras.datasets.coco.evaluation import evaluate_coco
 
 
 def train_config(args):
+    config = CocoConfig()
+    # FIXME move it
+    # FIXME apecify which model is for which dataset
+    from kernelphysiology.dl.keras.models import mrcnn as modellib
+    import os
+    args.save_dir = '/home/arash/Software/repositories/'
+    args.model_name = 'mrcnn'
+    args.log_dir = os.path.join(args.save_dir, args.model_name)
+    if not os.path.isdir(args.log_dir):
+        os.mkdir(args.log_dir)
+    model = modellib.MaskRCNN(mode='training', config=config, model_dir=args.log_dir)
+    
+    # Training dataset. Use the training set and 35K from the
+    # validation set, as as in the Mask RCNN paper.
+    dataset_train = CocoDataset()
+    dataset_train.load_coco(args.validation_dir, 'train', year=2017, auto_download=False)
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = CocoDataset()
+    val_type = 'val'
+    dataset_val.load_coco(args.validation_dir, val_type, year=2017, auto_download=False)
+    dataset_val.prepare()
+
+    # Image Augmentation
+    # Right/Left flip 50% of the time
+    import imgaug
+    augmentation = imgaug.augmenters.Fliplr(0.5)
+
+    # *** This training schedule is an example. Update to your needs ***
+
+    # Training - Stage 1
+    print("Training network heads")
+    model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE, epochs=40, layers='heads', augmentation=augmentation)
+
+    # Training - Stage 2
+    # Finetune layers from ResNet stage 4 and up
+    print("Fine tune Resnet stage 4 and up")
+    model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE, epochs=120, layers='4+', augmentation=augmentation)
+
+    # Training - Stage 3
+    # Fine tune all layers
+    print("Fine tune all layers")
+    model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE / 10, epochs=160, layers='all', augmentation=augmentation)
     return args
 
 
