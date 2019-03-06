1eca2df953a87fd1df7a6d3cd42aaae947e9eb80
Arash <akbarinia.arash@gmail.com>
2019-03-06 10:56:53 +0100

    removed gpus default for awesome

--- python/src/kernelphysiology/dl/keras/datasets/commons.py
+++ python/src/kernelphysiology/dl/keras/datasets/commons.py
@@ -5,7 +5,6 @@ The common files used in dataset.
 
 import os
 import sys
-import socket
 
 
 # finding the root of the project
@@ -15,7 +14,3 @@ project_dir = current_path.split(python_root, 1)[0]
 python_root = os.path.join(project_dir, python_root)
 project_dir = os.path.join(project_dir, 'kernelphysiology')
 sys.path += [os.path.join(python_root, 'src/')]
-
-if socket.gethostname() == 'awesome':
-    # list of GPUs allocated to me
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
