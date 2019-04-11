2420a175e90a9b69da6ee6f74dba22677d27d765
Arash <akbarinia.arash@gmail.com>
2019-04-11 18:15:37 +0200

    added prefix to script experiment names

--- python/src/kernelphysiology/dl/pytorch/scripts/experiment_noise.sh
+++ python/src/kernelphysiology/dl/pytorch/scripts/experiment_noise.sh
@@ -2,10 +2,11 @@ DATASET=$1
 NETWORKS=$2
 GPU_NUMBER=$3
 WORKERS=$4
+NAME_SUFFIX=$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --s_p_noise 0.01 0.05 0.1 --experiment_name s_p_noise
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --s_p_noise 0.01 0.05 0.1 --experiment_name s_p_noise_$NAME_SUFFIX
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --speckle_noise 0.01 0.05 0.1 --experiment_name speckle_noise
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --speckle_noise 0.01 0.05 0.1 --experiment_name speckle_noise_$NAME_SUFFIX
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --gaussian_noise 0.01 0.05 0.1 --experiment_name gaussian_noise
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --gaussian_noise 0.01 0.05 0.1 --experiment_name gaussian_noise_$NAME_SUFFIX
 
