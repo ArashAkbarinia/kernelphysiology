1ac9250de3d586756047242c14f396a01bd98c26
Arash Akbarinia <akbarinia.arash@gmail.com>
2019-04-15 15:04:44 +0200

    differentiating between original colour of a network and reductin of colour

--- python/src/kernelphysiology/analysis/reports/reports.py
+++ python/src/kernelphysiology/analysis/reports/reports.py
@@ -14,48 +14,55 @@ from kernelphysiology.analysis.utils.organise_results import \
 
 
 def do_one_network(result_dir, which_experiments, original_values, class_inds,
-                   which_categories={}, which_networks=None):
+                   which_categories=None, which_networks=None):
     # reading the results for original images
-    for experiment_dir in glob.glob(result_dir + '/original/'):
-        original_networks = {}
-
-        num_networks = len(glob.glob(experiment_dir + '/*/'))
-        for network_dir in sorted(glob.glob(experiment_dir + '/*/')):
-            network_name = get_folder_name(network_dir)
-            if which_networks is not None and not network_name in which_networks:
-                continue
-            num_tests = len(glob.glob(network_dir + '/*.csv'))
-            original_networks[network_name] = ResultSummary(network_name, 1,
-                                                            class_inds)
-            for prediction_file in sorted(glob.glob(network_dir + '/*.csv')):
-                predictions = np.loadtxt(prediction_file, delimiter=',')
-                original_networks[network_name].add_test(predictions, 0)
+    if which_categories is None:
+        which_categories = {}
+    experiment_dir = result_dir + '/original/'
+    original_results = read_experiment(experiment_dir, which_networks,
+                                       class_inds)
+    experiment_dir = result_dir + '/original_rgb/'
+    original_rgb_results = read_experiment(experiment_dir, which_networks,
+                                           class_inds)
 
     summary_out = {}
     for w_ind, experiment_name in enumerate(which_experiments):
-        networks = {}
-
         experiment_dir = result_dir + '/' + experiment_name + '/'
-        for network_dir in sorted(glob.glob(experiment_dir + '/*/')):
-            network_name = get_folder_name(network_dir)
-            if which_networks is not None and not network_name in which_networks:
-                continue
-            num_tests = len(glob.glob(network_dir + '/*.csv'))
-            networks[network_name] = ResultSummary(network_name, num_tests,
-                                                   class_inds)
-            for prediction_file in sorted(glob.glob(network_dir + '/*.csv')):
-                predictions = np.loadtxt(prediction_file, delimiter=',')
-                test_name = get_folder_name(prediction_file)
-                test_value = test_value_from_path(test_name[:-4])
-                networks[network_name].add_test(predictions, test_value)
+        other_results = read_experiment(experiment_dir, which_networks,
+                                        class_inds)
 
-        (all_xs, all_ys) = plot_results(networks, original_networks,
+        original_plot_results = original_results
+        if experiment_name in ['reduce_chromaticity',
+                               'red_green',
+                               'yellow_blue']:
+            original_plot_results = original_rgb_results
+        (all_xs, all_ys) = plot_results(other_results, original_plot_results,
                                         experiment_name, '_all',
                                         original_values[w_ind])
         for cat_name, cat_val in which_categories.items():
-            (_, _) = plot_results(networks, original_networks,
+            (_, _) = plot_results(other_results, original_plot_results,
                                   experiment_name, '_' + cat_name,
                                   original_values[w_ind],
                                   cat_val)
         summary_out[experiment_name] = (all_xs, all_ys)
     return summary_out
+
+
+def read_experiment(experiment_dir, which_networks, class_inds):
+    network_results = {}
+
+    for network_dir in sorted(glob.glob(experiment_dir + '/*/')):
+        network_name = get_folder_name(network_dir)
+        if which_networks is not None and network_name not in which_networks:
+            continue
+        num_tests = len(glob.glob(network_dir + '/*.csv'))
+        test_value = 0
+        network_results[network_name] = ResultSummary(network_name, num_tests,
+                                                      class_inds)
+        for prediction_file in sorted(glob.glob(network_dir + '/*.csv')):
+            predictions = np.loadtxt(prediction_file, delimiter=',')
+            if num_tests > 1:
+                test_name = get_folder_name(prediction_file)
+                test_value = test_value_from_path(test_name[:-4])
+            network_results[network_name].add_test(predictions, test_value)
+    return network_results
