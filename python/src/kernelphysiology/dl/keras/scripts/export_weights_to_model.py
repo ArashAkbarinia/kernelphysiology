0b2ca1dc914c9b52de01d65b6c38737d89fb74ac
Arash Akbarinia <akbarinia.arash@gmail.com>
2019-05-06 10:24:03 +0200

    removed unused common

--- python/src/kernelphysiology/dl/keras/scripts/export_weights_to_model.py
+++ python/src/kernelphysiology/dl/keras/scripts/export_weights_to_model.py
@@ -1,17 +1,14 @@
-'''
+"""
 The main function for the export_weights_to_model.sh.
-'''
+"""
 
-
-import commons
 import sys
 
-
 from kernelphysiology.dl.keras.models.utils import export_weights_to_model
-
 
 weights_path = sys.argv[1]
 model_path = sys.argv[2]
 architecture = sys.argv[3]
 dataset = sys.argv[4]
-export_weights_to_model(weights_path, model_path, architecture, dataset, area1layers=None)
+export_weights_to_model(weights_path, model_path, architecture, dataset,
+                        area1layers=None)
