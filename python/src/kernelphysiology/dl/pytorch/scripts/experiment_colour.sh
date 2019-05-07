910d259ada0f72ba2604541d623c95524e8b3d53
Arash <akbarinia.arash@gmail.com>
2019-05-07 14:32:13 +0200

    prediction saved in an organised manner according to type of manipulation

--- python/src/kernelphysiology/dl/pytorch/scripts/experiment_colour.sh
+++ python/src/kernelphysiology/dl/pytorch/scripts/experiment_colour.sh
@@ -2,18 +2,17 @@ DATASET=$1
 NETWORKS=$2
 GPU_NUMBER=$3
 WORKERS=$4
-NAME_SUFFIX=$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --chromaticity 0 0.1 0.25 0.50 --experiment_name reduce_chromaticity_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --chromaticity 0 0.1 0.25 0.50 --experiment_name ex_$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --red_green 0 0.1 0.25 0.50 --experiment_name red_green_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --red_green 0 0.1 0.25 0.50 --experiment_name ex_$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --yellow_blue 0 0.1 0.25 0.50 --experiment_name yellow_blue_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --yellow_blue 0 0.1 0.25 0.50 --experiment_name ex_$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --lightness 0 0.1 0.25 0.50 --experiment_name reduce_lightness_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --lightness 0 0.1 0.25 0.50 --experiment_name ex_$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --invert_chromaticity --experiment_name invert_chromaticity_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --invert_chromaticity --experiment_name ex_$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --invert_opponency --experiment_name invert_opponency_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --invert_opponency --experiment_name ex_$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --invert_lightness --experiment_name invert_lightness_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --invert_lightness --experiment_name ex_$5
--- python/src/kernelphysiology/dl/pytorch/scripts/experiment_original.sh
+++ python/src/kernelphysiology/dl/pytorch/scripts/experiment_original.sh
@@ -2,6 +2,6 @@ DATASET=$1
 NETWORKS=$2
 GPU_NUMBER=$3
 WORKERS=$4
-NAME_SUFFIX=$5
 
-python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --experiment_name original_$NAME_SUFFIX
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --experiment_name ex_$5
+
