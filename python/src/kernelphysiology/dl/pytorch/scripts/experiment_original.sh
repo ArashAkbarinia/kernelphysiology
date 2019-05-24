82f510efbab4741617dfa0672e1b6ef59358ca51
Arash <akbarinia.arash@gmail.com>
2019-05-24 11:22:22 +0200

    added test for original rgb images

--- python/src/kernelphysiology/dl/pytorch/scripts/experiment_original.sh
+++ python/src/kernelphysiology/dl/pytorch/scripts/experiment_original.sh
@@ -5,3 +5,5 @@ WORKERS=$4
 
 python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --experiment_name ex_$5
 
+python3 predict_image_classification.py $DATASET $NETWORKS --gpus $GPU_NUMBER --workers $WORKERS --original_rgb --experiment_name ex_$5
+
