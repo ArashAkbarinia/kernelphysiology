5e44a7f5e34d40ac2af2378894819fbea0e35c88
Arash <akbarinia.arash@gmail.com>
2019-07-25 11:14:39 +0200

    allowing different values for contrast in kill kernels

--- python/src/kernelphysiology/dl/pytorch/scripts/kill_kernels.sh
+++ python/src/kernelphysiology/dl/pytorch/scripts/kill_kernels.sh
@@ -7,6 +7,8 @@ START_KERNEL=$6
 START_KERNEL=${START_KERNEL:=0}
 EXPERIMENT_NAME=$7
 EXPERIMENT_NAME=${EXPERIMENT_NAME:="kill_convs"}
+CONTRAST_VALUE=$8
+CONTRAST_VALUE=${CONTRAST_VALUE:=0.3}
 
 
 for (( i=START_KERNEL; i<$NUM_KERNELS; i++ ))
@@ -14,8 +16,17 @@ for (( i=START_KERNEL; i<$NUM_KERNELS; i++ ))
     echo "Killing Kernel index "$i;
     KERNEL_IND_PRINT=$(printf "%04d" $i)
     CURRENT_NAME="l_"$LAYER_NAME"_k_"$KERNEL_IND_PRINT
-    > $EXPERIMENT_NAME"_"$GPU_NUMBER".txt";
-    echo $NET_CONTRAST","$CHROMATICITY","$CURRENT_NAME >> $EXPERIMENT_NAME"_"$GPU_NUMBER".txt"
-    python predict_image_classification.py imagenet $EXPERIMENT_NAME"_"$GPU_NUMBER".txt" --experiment_name $EXPERIMENT_NAME --gpus $GPU_NUMBER -j 8 -b 512 --kill_kernels $LAYER_NAME $i --contrasts 0.3 
-    python predict_image_classification.py imagenet $EXPERIMENT_NAME"_"$GPU_NUMBER".txt" --experiment_name $EXPERIMENT_NAME --gpus $GPU_NUMBER -j 8 -b 512 --kill_kernels $LAYER_NAME $i
+    > $EXPERIMENT_NAME".txt";
+    echo $NET_CONTRAST","$CHROMATICITY","$CURRENT_NAME >> $EXPERIMENT_NAME".txt"
+    FILE=$EXPERIMENT_NAME"/imagenet/contrast/"$CURRENT_NAME"/"$CURRENT_NAME"_contrast_"$CONTRAST_VALUE".csv"
+    if [ ! -f "$FILE" ]; then
+        echo "$FILE "
+        python predict_image_classification.py imagenet $EXPERIMENT_NAME".txt" --experiment_name $EXPERIMENT_NAME --gpus $GPU_NUMBER -j 8 -b 512 --kill_kernels $LAYER_NAME $i --contrasts $CONTRAST_VALUE 
+    fi
+
+    FILE=$EXPERIMENT_NAME"/imagenet/original/"$CURRENT_NAME"/"$CURRENT_NAME"_original_1.csv"
+    if [ ! -f "$FILE" ]; then
+        echo "$FILE "
+        python predict_image_classification.py imagenet $EXPERIMENT_NAME".txt" --experiment_name $EXPERIMENT_NAME --gpus $GPU_NUMBER -j 8 -b 512 --kill_kernels $LAYER_NAME $i
+    fi
 done
