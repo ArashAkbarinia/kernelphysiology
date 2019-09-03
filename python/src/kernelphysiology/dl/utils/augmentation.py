e828a3706cda678b04495461d7eb45c5274d40f3
Arash Akbarinia <akbarinia.arash@gmail.com>
2019-09-03 16:23:34 +0200

    minor commit

--- python/src/kernelphysiology/dl/utils/augmentation.py
+++ python/src/kernelphysiology/dl/utils/augmentation.py
@@ -4,11 +4,11 @@ Settings related to data augmentation.
 
 from kernelphysiology.utils import imutils
 
-supported_image_manipulations = {
+supported_training_manipulations = {
     'contrast': imutils.adjust_contrast,
     'gamma': imutils.adjust_gamma
 }
 
 
-def get_supported_image_manipulations():
-    return supported_image_manipulations
+def get_training_augmentations():
+    return supported_training_manipulations
