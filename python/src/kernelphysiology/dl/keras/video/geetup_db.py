b2a0233739fc30eeeb5b3962b563a62267b42a6f
Arash Akbarinia <akbarinia.arash@gmail.com>
2019-09-30 18:08:15 +0200

    moved non-keras codes of geetup out.

--- python/src/kernelphysiology/dl/keras/video/geetup_db.py
+++ python/src/kernelphysiology/dl/keras/video/geetup_db.py
@@ -3,111 +3,12 @@ Reading the GEETUP dataset and creating train and validation sets.
 """
 
 import numpy as np
-import glob
-import logging
-import os
 
 import keras
 import keras.backend as K
 from keras.preprocessing import image
 
 from kernelphysiology.filterfactory import gaussian
-from kernelphysiology.utils import path_utils
-
-
-def cleanup_dataset(dataset_dir):
-    for subject_dir in glob.glob(dataset_dir + '/*/'):
-        cleanup_subject(subject_dir)
-
-
-def cleanup_subject(subject_dir):
-    rows = 360
-    cols = 640
-    margin = 5
-    for segment in glob.glob(subject_dir + '/segments/*/'):
-        for video_dir in glob.glob(segment + '/CutVid_*/'):
-            good_frames_dir = video_dir + '/selected_frames/'
-            path_utils.create_dir(good_frames_dir)
-            current_num_frames = len(glob.glob((video_dir + '/*.jpg')))
-            video_ind = video_dir.split('/')[-2].split('_')[-1]
-            gts = np.loadtxt(
-                segment + '/SUBSAMP_EYETR_' + video_ind + '.txt')
-            if gts.shape[0] != current_num_frames:
-                logging.info('%s contains %d frames but %d fixation points' %
-                             (video_dir, current_num_frames, gts.shape[0]))
-                ignore_video_dir = '%s/Ignore_CutVid_%s/' % (segment, video_ind)
-                os.rename(video_dir, ignore_video_dir)
-                continue
-            # ignoring all fixation points that are around corner
-            conds = np.zeros((gts.shape[0], 4))
-            conds[:, 0] = np.all(
-                [[gts[:, 0] < margin], [gts[:, 1] < margin]],
-                axis=0
-            ).squeeze()
-            conds[:, 1] = np.all(
-                [[gts[:, 0] < margin], [gts[:, 1] > (rows - margin)]],
-                axis=0
-            ).squeeze()
-            conds[:, 2] = np.all(
-                [[gts[:, 0] > (cols - margin)], [gts[:, 1] < margin]],
-                axis=0
-            ).squeeze()
-            conds[:, 3] = np.all(
-                [[gts[:, 0] > (cols - margin)], [gts[:, 1] > (rows - margin)]],
-                axis=0
-            ).squeeze()
-            conds = np.invert(np.any(conds, axis=1))
-
-            j = 1
-            cleaned_gts = []
-            for i in range(current_num_frames):
-                previous_frame = True
-                next_frame = True
-                if i != 0:
-                    previous_frame = conds[i - 1]
-                if i != (current_num_frames - 1):
-                    next_frame = conds[i + 1]
-                if previous_frame and conds[i] and next_frame:
-                    # moving good frames to selected frames
-                    os.rename(
-                        video_dir + '/frames%d.jpg' % (i + 1),
-                        good_frames_dir + '/frames%d.jpg' % j,
-                    )
-                    j += 1
-                    cleaned_gts.append(gts[i, :])
-            np.savetxt(good_frames_dir + '/gt.txt', np.array(cleaned_gts))
-
-
-def last_valid_frame(video_dir, frames_gap=10, sequence_length=9):
-    """
-    Given the length of sequence and gap between frames, it computes which is
-    the last valid frame to read from a directory.
-    """
-    num_frames = len(glob.glob((video_dir + '/*.jpg')))
-    last_frame = num_frames - (frames_gap * sequence_length)
-    return last_frame
-
-
-def subject_frame_limits(subject_dir, frames_gap=10, sequence_length=9):
-    subject_data = []
-    for segment in sorted(glob.glob(subject_dir + '/segments/*/')):
-        for video_dir in sorted(glob.glob(segment + '/CutVid_*/')):
-            video_ind = video_dir.split('/')[-2].split('_')[-1]
-            current_video_limit = last_valid_frame(
-                video_dir + '/selected_frames/', frames_gap, sequence_length
-            )
-            if current_video_limit > 0:
-                subject_data.append([segment, video_ind, current_video_limit])
-    return subject_data
-
-
-def dataset_frame_list(dataset_dir, frames_gap=10, sequence_length=9):
-    dataset_data = []
-    for subject_dir in glob.glob(dataset_dir + '/*/'):
-        subject_data = subject_frame_limits(subject_dir, frames_gap=frames_gap,
-                                            sequence_length=sequence_length)
-        dataset_data.extend(subject_data)
-    return dataset_data
 
 
 def heat_map_from_fixation(fixation_point, target_size, gaussian_kernel=None,
@@ -253,7 +154,7 @@ class GeetupGenerator(keras.utils.Sequence):
             # get the video and frame number
             segment_dir = self.video_list[video_id[0]][0]
             video_num = self.video_list[video_id[0]][1]
-            selected_path =  segment_dir + '/CutVid_' + video_num + \
+            selected_path = segment_dir + '/CutVid_' + video_num + \
                             '/selected_frames/'
             video_path = selected_path + 'frames'
             fixation_path = selected_path + 'gt.txt'
