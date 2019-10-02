c3326789ce15abea17936c13cb46b8cbb9a3f542
Arash Akbarinia <akbarinia.arash@gmail.com>
2019-10-02 14:55:18 +0200

    inverse of normalisation pytorch.

--- python/src/kernelphysiology/dl/pytorch/utils/misc.py
+++ python/src/kernelphysiology/dl/pytorch/utils/misc.py
@@ -5,7 +5,26 @@ Miscellaneous utility functions and classes.
 import os
 import time
 import shutil
+
 import torch
+import torchvision
+
+
+class NormalizeInverse(torchvision.transforms.Normalize):
+    """
+    Undoes the normalization and returns the reconstructed images in the input
+    domain.
+    """
+
+    def __init__(self, mean, std):
+        mean = torch.as_tensor(mean)
+        std = torch.as_tensor(std)
+        std_inv = 1 / (std + 1e-7)
+        mean_inv = -mean * std_inv
+        super().__init__(mean=mean_inv, std=std_inv)
+
+    def __call__(self, tensor):
+        return super().__call__(tensor.clone())
 
 
 class AverageMeter(object):
