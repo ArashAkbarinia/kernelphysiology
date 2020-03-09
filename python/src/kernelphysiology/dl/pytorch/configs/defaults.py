5d81a59b53a441b877cfaa254fe3f1ed1dacd294
Arash Akbarinia <akbarinia.arash@gmail.com>
2020-03-09 10:43:11 +0100

    typo in the name of opponent

--- python/src/kernelphysiology/dl/pytorch/configs/defaults.py
+++ python/src/kernelphysiology/dl/pytorch/configs/defaults.py
@@ -83,7 +83,7 @@ _C.INPUT.MASK_FORMAT = "polygon"  # alternative: "bitmask"
 ###
 _C.INPUT.VISION_TYPE = "trichromat"
 _C.INPUT.CONTRAST = 1.0
-_C.INPUT.OPONNENT_SPACE = "lab"
+_C.INPUT.OPPONNENT_SPACE = "lab"
 
 
 # -----------------------------------------------------------------------------
