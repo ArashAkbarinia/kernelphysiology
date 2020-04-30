e1b97e2252d2c42d26ce4793c4ce6e2a3247653c
Arash Akbarinia <akbarinia.arash@gmail.com>
2020-04-30 18:05:29 +0200

    export images properly!

--- python/src/kernelphysiology/dl/pytorch/vaes/export_imgs.py
+++ python/src/kernelphysiology/dl/pytorch/vaes/export_imgs.py
@@ -6,136 +6,177 @@ import torch
 from torchvision import transforms
 
 from skimage import io
+import cv2
+from kernelphysiology.dl.pytorch.vaes import data_loaders
 
-from kernelphysiology.dl.pytorch.vaes import model
+from kernelphysiology.dl.pytorch.vaes import model as vqmodel
 from kernelphysiology.dl.pytorch.utils.preprocessing import inv_normalise_tensor
 from kernelphysiology.transformations import colour_spaces
 
-main_dir = '/home/arash/Desktop/nips/'
-
-batch_size = int(sys.argv[2])
-colour_space = sys.argv[1]
-model_path = main_dir + 'models/' + colour_space + '/checkpoints/model_40.pth'
-weights_rgb = torch.load(model_path, map_location='cpu')
-
-net_rgb = model.VQ_CVAE(128, k=512, in_chns=3)
-net_rgb.load_state_dict(weights_rgb)
-
 from kernelphysiology.dl.pytorch.utils import cv2_transforms
 from kernelphysiology.dl.pytorch.utils import preprocessing
+from kernelphysiology.transformations import normalisations
+import argparse
 
-mean = (0.5, 0.5, 0.5)
-std = (0.5, 0.5, 0.5)
-transform_funcs = transforms.Compose(
-    [cv2_transforms.Resize(256), cv2_transforms.CenterCrop(224),
-     cv2_transforms.ToTensor(),
-     cv2_transforms.Normalize(mean, std)
-     ])
 
-intransform_funs = []
-mosaic_pattern = None
-if mosaic_pattern is not None:
-    intransform_funs.append(
-        preprocessing.MosaicTransformation(mosaic_pattern)
+def parse_arguments(args):
+    parser = argparse.ArgumentParser(description='Variational AutoEncoders')
+    parser.add_argument(
+        '--model_path',
+        help='autoencoder variant to use: vae | vqvae'
     )
-intransform = transforms.Compose(intransform_funs)
-outtransform_funs = []
-inv_func = None
-colour_space_lab = 'lab'
-if colour_space_lab is not None:
-    outtransform_funs.append(
-        preprocessing.ColourTransformation(None, colour_space_lab)
+    parser.add_argument(
+        '--batch_size', type=int, default=128, metavar='N',
+        help='input batch size for training (default: 128)'
     )
-outtransform_lab = transforms.Compose(outtransform_funs)
+    parser.add_argument('--k', type=int, dest='k', metavar='K',
+                        help='number of atoms in dictionary')
+    parser.add_argument('--exclude', type=int, default=0, metavar='K',
+                        help='number of atoms in dictionary')
+    parser.add_argument('--colour_space', type=str, default=None,
+                        help='The type of output colour space.')
 
-outtransform_funs_hsv = []
-colour_space_hsv = 'hsv'
-if colour_space_hsv is not None:
-    outtransform_funs_hsv.append(
-        preprocessing.ColourTransformation(None, colour_space_hsv)
+    parser.add_argument(
+        '--dataset', default=None,
+        help='dataset to use: mnist | cifar10 | imagenet | coco | custom'
     )
-outtransform_hsv = transforms.Compose(outtransform_funs_hsv)
+    parser.add_argument(
+        '--category',
+        type=str,
+        default=None,
+        help='The specific category (default: None)'
+    )
+    parser.add_argument(
+        '--validation_dir',
+        type=str,
+        default=None,
+        help='The path to the validation directory (default: None)'
+    )
+    parser.add_argument(
+        '--out_dir',
+        type=str,
+        default=None,
+        help='The path to the validation directory (default: None)'
+    )
 
-import cv2
+    return parser.parse_args(args)
 
-out_dir = main_dir + '/outs/'
 
-imagenet_path = '/home/arash/Software/imagenet/'
-validation_set = imagenet_path + 'raw-data/validation/'
+def main(args):
+    args = parse_arguments(args)
+    weights_rgb = torch.load(args.model_path, map_location='cpu')
+    network = vqmodel.VQ_CVAE(128, k=args.k, in_chns=3)
+    network.load_state_dict(weights_rgb)
+    if args.exclude > 0:
+        which_vec = [args.exclude - 1]
+        print(which_vec)
+        network.state_dict()['emb.weight'][:, which_vec] = 0
+    elif args.exclude < 0:
+        which_vec = [*range(8)]
+        which_vec.remove(abs(args.exclude) - 1)
+        print(which_vec)
+        network.state_dict()['emb.weight'][:, which_vec] = 0
+    network.cuda()
+    network.eval()
 
-# net_rgb.cuda()
-net_rgb.eval()
+    if not os.path.exists(args.out_dir):
+        os.mkdir(args.out_dir)
 
-org_img = []
-rec_rgb = []
-diff_rgb = []
+    mean = (0.5, 0.5, 0.5)
+    std = (0.5, 0.5, 0.5)
+    transform_funcs = transforms.Compose([
+        # cv2_transforms.Resize(256), cv2_transforms.CenterCrop(224),
+        cv2_transforms.ToTensor(),
+        cv2_transforms.Normalize(mean, std)
+    ])
 
-import data_loaders
+    if args.dataset == 'imagenet':
+        test_loader = torch.utils.data.DataLoader(
+            data_loaders.ImageFolder(
+                root=args.validation_dir,
+                intransform=None,
+                outtransform=None,
+                transform=transform_funcs
+            ),
+            batch_size=args.batch_size, shuffle=False
+        )
+    else:
+        test_loader = torch.utils.data.DataLoader(
+            data_loaders.CategoryImages(
+                root=args.validation_dir,
+                # FIXME
+                category=args.category,
+                intransform=None,
+                outtransform=None,
+                transform=transform_funcs
+            ),
+            batch_size=args.batch_size, shuffle=False
+        )
+    export(test_loader, network, mean, std, args)
 
-test_loader = torch.utils.data.DataLoader(
-    data_loaders.ImageFolder(
-        root=validation_set,
-        intransform=None,
-        outtransform=None,
-        transform=transform_funcs
-    ),
-    batch_size=batch_size, shuffle=False
-)
-with torch.no_grad():
-    for i, (img_readies, img_target, img_paths) in enumerate(test_loader):
-        img_readies = img_readies  # .cuda()
-        out_rgb = net_rgb(img_readies)
-        out_rgb = out_rgb[0].detach().cpu()
-        img_readies = img_readies.detach().cpu()
 
-        for img_ind in range(out_rgb.shape[0]):
-            img_path = img_paths[img_ind]
-            img_ready = img_readies[img_ind].unsqueeze(0)
+def export(data_loader, model, mean, std, args):
+    diffs = []
+    with torch.no_grad():
+        for i, (img_readies, img_target, img_paths) in enumerate(data_loader):
+            img_readies = img_readies.cuda()
+            out_rgb = model(img_readies)
+            out_rgb = out_rgb[0].detach().cpu()
+            img_readies = img_readies.detach().cpu()
 
-            cat_in_dir = os.path.dirname(img_path) + '/'
+            for img_ind in range(out_rgb.shape[0]):
+                img_path = img_paths[img_ind]
+                img_ready = img_readies[img_ind].unsqueeze(0)
 
-            cat_out_dir = cat_in_dir.replace(validation_set, out_dir)
-            if not os.path.exists(cat_out_dir):
-                os.mkdir(cat_out_dir)
+                cat_in_dir = os.path.dirname(img_path) + '/'
 
-            org_img_tmp = inv_normalise_tensor(img_ready, mean, std)
-            org_img_tmp = org_img_tmp.numpy().squeeze().transpose(1, 2, 0)
-            org_img_tmp = org_img_tmp * 255
-            org_img_tmp = org_img_tmp.astype('uint8')
-            # org_img.append(org_img_tmp)
+                cat_out_dir = cat_in_dir.replace(args.validation_dir,
+                                                 args.out_dir)
+                if not os.path.exists(cat_out_dir):
+                    os.mkdir(cat_out_dir)
 
-            org_dir = cat_out_dir + '/org/'
-            if not os.path.exists(org_dir):
-                os.mkdir(org_dir)
-            rgb_dir = cat_out_dir + '/' + colour_space[4:] + '/'
-            if not os.path.exists(rgb_dir):
-                os.mkdir(rgb_dir)
+                org_img_tmp = inv_normalise_tensor(img_ready, mean, std)
+                org_img_tmp = org_img_tmp.numpy().squeeze().transpose(1, 2, 0)
+                org_img_tmp = org_img_tmp * 255
+                org_img_tmp = org_img_tmp.astype('uint8')
+                # org_img.append(org_img_tmp)
 
-            if os.path.exists(img_path.replace(cat_in_dir, rgb_dir)):
-                rec_rgb_tmp = cv2.imread(img_path.replace(cat_in_dir, rgb_dir))
-                rec_rgb_tmp = cv2.cvtColor(rec_rgb_tmp, cv2.COLOR_BGR2RGB)
-            else:
-                rec_rgb_tmp = inv_normalise_tensor(
+                rec_dir = cat_out_dir + '/' + args.colour_space + '/'
+                if not os.path.exists(rec_dir):
+                    os.mkdir(rec_dir)
+
+                # if os.path.exists(img_path.replace(cat_in_dir, rgb_dir)):
+                #     rec_rgb_tmp = cv2.imread(
+                #         img_path.replace(cat_in_dir, rgb_dir))
+                #     rec_rgb_tmp = cv2.cvtColor(rec_rgb_tmp, cv2.COLOR_BGR2RGB)
+                # else:
+                rec_img_tmp = inv_normalise_tensor(
                     out_rgb[img_ind].unsqueeze(0), mean, std)
-                rec_rgb_tmp = rec_rgb_tmp.numpy().squeeze().transpose(1, 2, 0)
-                if colour_space == 'rgb2lab':
-                    rec_rgb_tmp = rec_rgb_tmp * 255
-                    rec_rgb_tmp = rec_rgb_tmp.astype('uint8')
-                    rec_rgb_tmp = cv2.cvtColor(rec_rgb_tmp, cv2.COLOR_LAB2RGB)
-                elif colour_space == 'rgb2hsv':
-                    rec_rgb_tmp = rec_rgb_tmp * 255
-                    rec_rgb_tmp = rec_rgb_tmp.astype('uint8')
-                    rec_rgb_tmp = cv2.cvtColor(rec_rgb_tmp, cv2.COLOR_HSV2RGB)
-                elif colour_space == 'rgb2dkl':
-                    rec_rgb_tmp = colour_spaces.dkl012rgb(rec_rgb_tmp)
-            ## rec_rgb.append(rec_rgb_tmp)
+                rec_img_tmp = rec_img_tmp.numpy().squeeze().transpose(1, 2, 0)
+                rec_img_tmp = cv2.resize(
+                    rec_img_tmp, (org_img_tmp.shape[1], org_img_tmp.shape[0])
+                )
+                if args.colour_space == 'lab':
+                    rec_img_tmp = rec_img_tmp * 255
+                    rec_img_tmp = rec_img_tmp.astype('uint8')
+                    rec_img_tmp = cv2.cvtColor(rec_img_tmp, cv2.COLOR_LAB2RGB)
+                elif args.colour_space == 'hsv':
+                    rec_img_tmp = colour_spaces.hsv012rgb(rec_img_tmp)
+                elif args.colour_space == 'dkl':
+                    rec_img_tmp = colour_spaces.dkl012rgb(rec_img_tmp)
+                else:
+                    rec_img_tmp = normalisations.uint8im(rec_img_tmp)
 
-            tmp_org = org_img_tmp.astype('float') / 255
-            tmp_rgb = rec_rgb_tmp.astype('float') / 255
-            diff_rgb.append(np.array((tmp_org - tmp_rgb) ** 2).mean())
+                tmp_org = org_img_tmp.astype('float') / 255
+                tmp_rgb = rec_img_tmp.astype('float') / 255
+                diffs.append(np.array((tmp_org - tmp_rgb) ** 2).mean())
 
-            if not os.path.exists(img_path.replace(cat_in_dir, org_dir)):
-                io.imsave(img_path.replace(cat_in_dir, org_dir), org_img_tmp)
-            if not os.path.exists(img_path.replace(cat_in_dir, rgb_dir)):
-                io.imsave(img_path.replace(cat_in_dir, rgb_dir), rec_rgb_tmp)
-        np.savetxt('' + colour_space[4:] + '.txt', np.array(diff_rgb))
+                # if not os.path.exists(img_path.replace(cat_in_dir, rec_dir)):
+                print(img_path.replace(cat_in_dir, rec_dir))
+                io.imsave(img_path.replace(cat_in_dir, rec_dir), rec_img_tmp)
+            np.savetxt(args.out_dir + '/' + args.colour_space + '.txt',
+                       np.array(diffs))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
