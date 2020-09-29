7a9e1d4f03d9b8cf452eb377b7e2378e7ced7957
Arash Akbarinia <akbarinia.arash@gmail.com>
2020-09-29 10:56:09 +0200

    ssim and psnr

--- python/src/kernelphysiology/dl/pytorch/vaes/compare_org.py
+++ python/src/kernelphysiology/dl/pytorch/vaes/compare_org.py
@@ -4,6 +4,7 @@ import glob
 import numpy as np
 from skimage import io
 from skimage import color
+from skimage import metrics
 import cv2
 
 
@@ -34,6 +35,12 @@ def parse_arguments(args):
         default=None,
         help='The path to the validation directory (default: None)'
     )
+    parser.add_argument(
+        '--de',
+        action='store_true',
+        default=False,
+        help='Compute DeltaE (default: False)'
+    )
 
     return parser.parse_args(args)
 
@@ -44,6 +51,8 @@ def main(args):
     res_img_paths = sorted(glob.glob(args.res_dir + '/*.jpg'))
 
     all_des = []
+    all_ssim = []
+    all_psnr = []
     for i in range(len(org_img_paths)):
         print(i, org_img_paths[i])
         img_org = io.imread(org_img_paths[i])
@@ -54,13 +63,23 @@ def main(args):
         if img_org.shape != img_res.shape:
             img_org = cv2.resize(img_org, (img_res.shape[1], img_res.shape[0]))
 
-        img_org = color.rgb2lab(img_org)
-        img_res = color.rgb2lab(img_res)
-        de = color.deltaE_ciede2000(img_org, img_res)
-        all_des.append([np.mean(de), np.median(de), np.max(de)])
+        ssim = metrics.structural_similarity(img_org, img_res)
+        all_ssim.append(ssim)
+        psnr = metrics.peak_signal_noise_ratio(img_org, img_res)
+        all_psnr.append(psnr)
+        if args.de:
+            img_org = color.rgb2lab(img_org)
+            img_res = color.rgb2lab(img_res)
+            de = color.deltaE_ciede2000(img_org, img_res)
+            all_des.append([np.mean(de), np.median(de), np.max(de)])
 
-    np.savetxt(args.out_dir + '/' + args.colour_space + '.txt',
-               np.array(all_des))
+    np.savetxt(args.out_dir + '/ssim_' + args.colour_space + '.txt',
+               np.array(all_ssim))
+    np.savetxt(args.out_dir + '/psnr_' + args.colour_space + '.txt',
+               np.array(all_psnr))
+    if args.de:
+        np.savetxt(args.out_dir + '/' + args.colour_space + '.txt',
+                   np.array(all_des))
 
 
 if __name__ == "__main__":
