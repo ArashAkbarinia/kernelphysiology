b039d5ecd29bd92e39cf2fe260dc2cd3d158f4da
Arash Akbarinia <akbarinia.arash@gmail.com>
2020-10-19 17:23:20 +0200

    analaysing the csf activations

--- python/src/kernelphysiology/analysis/reports/csf_activations.py
+++ python/src/kernelphysiology/analysis/reports/csf_activations.py
@@ -4,19 +4,26 @@ import ntpath
 import os
 from matplotlib import pyplot as plt
 import sys
+from scipy import stats
 
 from kernelphysiology.utils import path_utils
 
 csf_dir = "/home/arash/Desktop/projects/csf/"
 activations_dir = "%s/data/kernel_activations/" % csf_dir
-out_dir = "%s/figures/activations/" % csf_dir
+fig_out_dir = "%s/figures/activations/" % csf_dir
+anl_out_dir = "%s/analysis/activation_corrs/" % csf_dir
 target_size = 256
 base_sf = ((target_size / 2) / np.pi)
 
 
+def get_human_csf(f):
+    return 2.6 * (0.0192 + 0.114 * f) * np.exp(-(0.114 * f) ** 1.1)
+
+
 def process_network(net_name):
     for file_path in sorted(
-            glob.glob(os.path.join(activations_dir, net_name) + '/*.pickle')):
+            glob.glob(os.path.join(activations_dir, net_name) + '/*.pickle')
+    ):
         file_name = ntpath.basename(file_path)
         layer_name = 'layer'
         name_parts = file_name.split('_')
@@ -24,26 +31,100 @@ def process_network(net_name):
             if part == 'layer':
                 layer_name += name_parts[pind + 1].replace('.conv2.weight', '')
                 break
-        png_name = os.path.join(out_dir, net_name,
-                                '%s_activation_%s.png' % (layer_name, 'max'))
-        if os.path.exists(png_name):
-            continue
+        png_name = os.path.join(
+            fig_out_dir, net_name, '%s_activation_%s.png' % (layer_name, 'max')
+        )
+        csv_name = os.path.join(
+            anl_out_dir, net_name, '%s_corrs_0.1.csv' % layer_name
+        )
         print('reading', file_name, layer_name)
         result_mat = path_utils.read_pickle(file_path)
         contrast_activation, xaxis = process_layer(result_mat)
-        plot_layer(contrast_activation, xaxis, net_name, layer_name)
+        if not os.path.exists(png_name):
+            plot_layer(contrast_activation, xaxis, net_name, layer_name)
+        if not os.path.exists(csv_name):
+            corr_layer(contrast_activation, xaxis, net_name, layer_name)
+        maxsf_layer(contrast_activation, xaxis, net_name, layer_name)
     return
 
 
 def plot_layer(contrast_activation, xaxis, net_name, layer_name):
-    report_types = ['lavg', 'lmed', 'lmax', 'ravg', 'rmed', 'rmax', 'avg',
-                    'med', 'max']
+    report_types = [
+        'lavg', 'lmed', 'lmax', 'ravg', 'rmed', 'rmax',
+        'avg', 'med', 'max', 'pavg', 'pmed', 'pmax'
+    ]
+    human_csf = np.array([get_human_csf(f) for f in xaxis])
     for report_key in report_types:
-        fig = plot_activations(contrast_activation, report_key, xaxis)
+        fig = plot_activations(
+            contrast_activation, report_key, xaxis, human_csf
+        )
         fig.tight_layout()
-        fig.savefig(os.path.join(out_dir, net_name, '%s_activation_%s.png' % (
-            layer_name, report_key)))
+        fig.savefig(
+            os.path.join(
+                fig_out_dir, net_name,
+                '%s_activation_%s.png' % (layer_name, report_key)
+            )
+        )
         plt.close('all')
+    return
+
+
+def corr_layer(contrast_activation, xaxis, net_name, layer_name):
+    report_types = [
+        'lavg', 'lmed', 'lmax', 'ravg', 'rmed', 'rmax',
+        'avg', 'med', 'max', 'pavg', 'pmed', 'pmax'
+    ]
+    human_csf = np.array([get_human_csf(f) for f in xaxis])
+
+    contrast_keys = list(contrast_activation.keys())
+    reports_cross = dict()
+    for ckey in contrast_keys:
+        reports_cross[ckey] = []
+
+    for report_key in report_types:
+        corrs = corr_activations(contrast_activation, report_key, human_csf)
+        for ckey, cval in corrs.items():
+            reports_cross[ckey].append(cval)
+    header = ','.join(e for e in report_types)
+    for ckey, cval in reports_cross.items():
+        out_file = os.path.join(
+            anl_out_dir, net_name, '%s_corrs_%s.csv' % (layer_name, ckey)
+        )
+        np.savetxt(out_file, np.array(cval).T, delimiter=',', header=header)
+    return
+
+
+def maxsf_layer(contrast_activation, xaxis, net_name, layer_name):
+    report_types = ['pavg', 'pmed', 'pmax']
+    human_csf = np.array([get_human_csf(f) for f in xaxis])
+
+    # plotting the peack frequencies
+    fig = plt.figure(figsize=(18, 6))
+    for i, report_key in enumerate(report_types):
+        maxsf = max_activations(contrast_activation, report_key)
+
+        ax = fig.add_subplot(1, 3, i + 1)
+        for ckey, cval in maxsf.items():
+            toplot = cval / cval.max()
+            p_corr, r_corr = stats.pearsonr(human_csf, np.array(toplot))
+            ax.plot(xaxis, toplot, '-x', label=ckey + ' - %.2f' % p_corr)
+        ax.plot(xaxis, human_csf, '--', color='black')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+
+        ax.legend()
+        ax.set_xlabel('SF')
+        ax.set_ylabel('#Kernels')
+        ax.set_title(report_key)
+
+    fig.tight_layout()
+    fig.savefig(
+        os.path.join(
+            fig_out_dir, 'peak_activations', net_name,
+            '%s_peakactivation.png' % (layer_name)
+        )
+    )
+    plt.close('all')
     return
 
 
@@ -57,7 +138,7 @@ def process_layer(result_mat):
         unique_frequencies = np.unique(val_contrast[:, 1])
         nkernels = val_contrast[0, 5].shape[0]
         nsamples = val_contrast.shape[0]
-        contrast_report = {
+        report = {
             'lavg': np.zeros((nsamples, nkernels)),
             'lmed': np.zeros((nsamples, nkernels)),
             'lmax': np.zeros((nsamples, nkernels)),
@@ -65,27 +146,31 @@ def process_layer(result_mat):
             'rmed': np.zeros((nsamples, nkernels)),
             'rmax': np.zeros((nsamples, nkernels)),
         }
-        rkeys = list(contrast_report.keys())
+        rkeys = list(report.keys())
 
         # create a numpy matrix out of all kernels
         for i in range(val_contrast.shape[0]):
             for j in range(len(rkeys)):
-                contrast_report[rkeys[j]][i] = val_contrast[i, j + 5]
+                report[rkeys[j]][i] = val_contrast[i, j + 5]
         # putting left and right together
-        contrast_report['avg'] = (contrast_report['lavg'] + contrast_report[
-            'ravg']) / 2
-        contrast_report['med'] = (contrast_report['lmed'] + contrast_report[
-            'rmed']) / 2
-        contrast_report['max'] = (contrast_report['lmax'] + contrast_report[
-            'rmax']) / 2
+        for pk in ['avg', 'med', 'max']:
+            report[pk] = (report['l' + pk] + report['r' + pk]) / 2
+
+        # activity at the side of where stimuli was presented
+        # sidef == 0 means the right side
+        cols = val_contrast[:, 4] == 0
+        for pk in ['avg', 'med', 'max']:
+            report['p' + pk] = report['l' + pk]
+            report['p' + pk][cols, :] = report['r' + pk][cols, :]
+
         # computing the average of each kernel for all samples
-        for rkey, rval in contrast_report.items():
+        for rkey, rval in report.items():
             activation_freq = []
             for freq in unique_frequencies:
-                condition = val_contrast[:, 1] == freq
-                activation_freq.append(np.mean(rval[condition, :], axis=0))
-            contrast_report[rkey] = activation_freq
-        contrast_activation[str(contrast)] = contrast_report
+                cols = val_contrast[:, 1] == freq
+                activation_freq.append(np.mean(rval[cols, :], axis=0))
+            report[rkey] = activation_freq
+        contrast_activation[str(contrast)] = report
 
     xaxis = [((1 / e) * base_sf) for e in unique_frequencies]
     return contrast_activation, xaxis
@@ -99,7 +184,7 @@ def extract_frequency(result_mat, frequency):
     return result_mat[result_mat[:, 1] == frequency, :]
 
 
-def plot_activations(activations, report_key, xaxis):
+def plot_activations(activations, report_key, xaxis, human_csf):
     contrast_keys = list(activations.keys())
     nkernels = activations[contrast_keys[0]][report_key][0].shape[0]
     rows = int(nkernels / 8)
@@ -111,14 +196,52 @@ def plot_activations(activations, report_key, xaxis):
             toplot = []
             for freq in activations[ckey][report_key]:
                 toplot.append(freq[i])
-            ax.plot(xaxis, toplot, '-x', label=ckey)
+            p_corr, r_corr = stats.pearsonr(human_csf, np.array(toplot))
+            ax.plot(xaxis, toplot, '-x', label=ckey + ' - %.2f' % p_corr)
         ax.set_xticklabels([])
         ax.set_yticklabels([])
-        if i == 0:
-            ax.legend()
-            ax.set_xlabel('SF')
-            ax.set_ylabel('Activation')
+
+        ax.legend()
+        ax.set_xlabel('SF')
+        ax.set_ylabel('Activation')
     return fig
+
+
+def corr_activations(activations, report_key, human_csf):
+    contrast_keys = list(activations.keys())
+    nkernels = activations[contrast_keys[0]][report_key][0].shape[0]
+
+    corrs = dict()
+    for ckey in contrast_keys:
+        corrs[ckey] = []
+    for i in range(nkernels):
+        for ckey in contrast_keys:
+            net_csf = []
+            for freq in activations[ckey][report_key]:
+                net_csf.append(freq[i])
+            p_corr, r_corr = stats.pearsonr(human_csf, np.array(net_csf))
+            corrs[ckey].append(p_corr)
+
+    return corrs
+
+
+def max_activations(activations, report_key):
+    contrast_keys = list(activations.keys())
+    nkernels = activations[contrast_keys[0]][report_key][0].shape[0]
+    nfreqs = len(activations[contrast_keys[0]][report_key])
+
+    maxsfs = dict()
+    for ckey in contrast_keys:
+        maxsfs[ckey] = np.zeros((nfreqs))
+    for i in range(nkernels):
+        for ckey in contrast_keys:
+            net_csf = []
+            for freq in activations[ckey][report_key]:
+                net_csf.append(freq[i])
+            max_ind = np.array(net_csf).argmax()
+            maxsfs[ckey][max_ind] += 1
+
+    return maxsfs
 
 
 if __name__ == "__main__":
