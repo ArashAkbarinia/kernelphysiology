271c3a2514cfd1521cbbaae5b0b25244abd76fb6
arash <akbarinia.arash@gmail.com>
2021-03-15 11:35:09 +0100

    removing set gpu in environments

--- python/src/kernelphysiology/utils/system_utils.py
+++ python/src/kernelphysiology/utils/system_utils.py
@@ -6,8 +6,9 @@ import os
 
 
 def set_visible_gpus(gpus):
-    if gpus[0] == -1 or gpus is None:
-        gpus = []
-    os.environ['CUDA_VISIBLE_DEVICES'] = ', '.join(str(e) for e in gpus)
-    gpus = [*range(len(gpus))]
+    # this is better done with CUDA_VISIBLE_DEVICES
+    # if gpus[0] == -1 or gpus is None:
+    #     gpus = []
+    # os.environ['CUDA_VISIBLE_DEVICES'] = ', '.join(str(e) for e in gpus)
+    # gpus = [*range(len(gpus))]
     return gpus
