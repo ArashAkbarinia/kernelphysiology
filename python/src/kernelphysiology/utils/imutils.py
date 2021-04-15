7faa2b1acc3fe208334447afe5b7e6e55f213404
arash <akbarinia.arash@gmail.com>
2021-04-15 19:08:19 +0200

    spatial frequency processing

--- python/src/kernelphysiology/utils/imutils.py
+++ python/src/kernelphysiology/utils/imutils.py
@@ -561,3 +561,55 @@ def mask_image(img, mask_val, **kwargs):
     mask_filter = create_mask_image(masked_img, **kwargs)
     masked_img[mask_filter == 1] = mask_val
     return masked_img
+
+
+def filter_img_sf(img, **kwargs):
+    img = img.astype('float') / 255
+    img_norm = (img.copy() - 0.5) / 0.5
+    if len(img_norm.shape) > 2:
+        img_back = np.zeros(img_norm.shape)
+        for i in range(img_norm.shape[2]):
+            img_back[:, :, i] = _filter_chn_sf(img_norm[:, :, i], **kwargs)
+    else:
+        img_back = _filter_chn_sf(img_norm, **kwargs)
+    img_back = (img_back * 0.5) + 0.5
+    img_back = img_back * 255
+    img_back = img_back.astype('uint8')
+    return img_back
+
+
+def _filter_chn_sf(img, **kwargs):
+    img_freq = np.fft.fft2(img)
+    img_freq_cent = np.fft.fftshift(img_freq)
+    img_sf_filtered = _cutoff_chn_fourier(img_freq_cent, **kwargs)
+
+    img_back = np.real(np.fft.ifft2(np.fft.ifftshift(img_sf_filtered)))
+    img_back[img_back < -1] = -1
+    img_back[img_back > 1] = 1
+    return img_back
+
+
+def _cutoff_chn_fourier(img, hsf_cut, lsf_cut):
+    rows = img.shape[0]
+    cols = img.shape[1]
+    smaller_side = np.minimum(rows, cols)
+    centre = (int(math.floor(cols / 2)), int(math.floor(rows / 2)))
+
+    if hsf_cut == 0:
+        mask_hsf = np.ones(img.shape, np.uint8)
+    else:
+        hsf_cut = 1 - hsf_cut
+        hsf_length = int(math.floor(hsf_cut * smaller_side * 0.5))
+        mask_hsf = np.zeros(img.shape, np.uint8)
+        mask_hsf = cv2.circle(mask_hsf, centre, hsf_length, (1, 1, 1), -1)
+
+    if lsf_cut == 0:
+        mask_lsf = np.ones(img.shape, np.uint8)
+    else:
+        lsf_length = int(math.floor(lsf_cut * smaller_side * 0.5))
+        mask_lsf = np.zeros(img.shape, np.uint8)
+        mask_lsf = 1 - cv2.circle(mask_lsf, centre, lsf_length, (1, 1, 1), -1)
+
+    mask_img = np.logical_and(mask_lsf, mask_hsf).astype('uint8')
+    img_sf_filtered = np.multiply(img, mask_img)
+    return img_sf_filtered
