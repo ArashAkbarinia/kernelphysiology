574631226ed8cf15061d9d15562798c2febe52b1
Arash Akbarinia <akbarinia.arash@gmail.com>
2022-03-04 10:55:30 +0100

    bg and shape type for train and test colour discrimination

--- python/src/kernelphysiology/dl/experiments/colour_discrimination/datasets/dataloader.py
+++ python/src/kernelphysiology/dl/experiments/colour_discrimination/datasets/dataloader.py
@@ -128,52 +128,68 @@ class OddOneOutVal(torch_data.Dataset):
         return len(self.data_file)
 
 
-class ShapeTrain(torch_data.Dataset):
-
-    def __init__(self, root, transform=None, colour_dist=None, background='rnd', **kwargs):
+class ShapeDataset(torch_data.Dataset):
+    def __init__(self, root, transform=None, background=None, same_shapes=None, **kwargs):
         self.root = root
         self.transform = transform
-        self.angles = (1, 11)
-        self.target_size = 224
+        self.target_size = (224, 224)
+        self.mask_size = (128, 128)
         self.imgdir = '%s/shape2D/' % self.root
-        self.img_paths = sorted(glob.glob(self.imgdir + '*.png'))
-        self.rgb_diffs, self.rgb_probs = _normal_dist_ints(25, scale=5)
-        self.colour_dist = colour_dist
         self.bg = background
-        if self.colour_dist is not None:
-            self.colour_dist = np.loadtxt(self.colour_dist, delimiter=',', dtype=int)
+        self.same_shapes = same_shapes
 
-    def _prepare_train_imgs(self, masks, others_colour, target_colour):
+    def _prepare_out_imgs(self, masks, others_colour, target_colour, place_fun):
         imgs = []
         for mask_ind, mask in enumerate(masks):
-            mask = cv2.resize(mask, (128, 128), interpolation=cv2.INTER_NEAREST)
+            mask = cv2.resize(mask, self.mask_size, interpolation=cv2.INTER_NEAREST)
             current_colour = target_colour if mask_ind == 0 else others_colour
-            # TODO: option for type of the background
             if os.path.exists(self.bg):
                 bg_img = io.imread(self.bg)
-                mask_img = cv2.resize(bg_img, mask.shape, interpolation=cv2.INTER_NEAREST)
-                img = cv2.resize(bg_img, (self.target_size, self.target_size),
-                                 interpolation=cv2.INTER_NEAREST)
+                mask_img = cv2.resize(bg_img, self.mask_size, interpolation=cv2.INTER_NEAREST)
+                img = cv2.resize(bg_img, self.target_size, interpolation=cv2.INTER_NEAREST)
             elif self.bg == 'rnd':
-                mask_img = np.random.randint(0, 256, (*mask.shape, 3), dtype='uint8')
-                img = np.random.randint(0, 256, (self.target_size, self.target_size, 3),
-                                        dtype='uint8')
+                mask_img = np.random.randint(0, 256, (*self.mask_size, 3), dtype='uint8')
+                img = np.random.randint(0, 256, (*self.target_size, 3), dtype='uint8')
             else:
-                mask_img = np.zeros((*mask.shape, 3), dtype='uint8') + 128
-                img = np.zeros((self.target_size, self.target_size, 3), dtype='uint8') + 128
+                mask_img = np.zeros((*self.mask_size, 3), dtype='uint8') + int(self.bg)
+                img = np.zeros((*self.target_size, 3), dtype='uint8') + int(self.bg)
+            # converting images to float
+            mask_img = mask_img.astype('float32') / 255
+            img = img.astype('float32') / 255
+
             for chn_ind in range(3):
                 current_chn = mask_img[:, :, chn_ind]
                 current_chn[mask == 255] = current_colour[chn_ind]
 
-            mask_size = mask.shape
-            srow = random.randint(0, self.target_size - mask_size[0])
-            erow = srow + mask_size[0]
-            scol = random.randint(0, self.target_size - mask_size[1])
-            ecol = scol + mask_size[1]
+            srow, scol = place_fun(self.mask_size, self.target_size)
+            erow = srow + self.mask_size[0]
+            ecol = scol + self.mask_size[1]
             img[srow:erow, scol:ecol] = mask_img
             imgs.append(img)
         if self.transform is not None:
             imgs = self.transform(imgs)
+        return imgs
+
+
+class ShapeTrain(ShapeDataset):
+
+    def __init__(self, root, transform=None, colour_dist=None, **kwargs):
+        ShapeDataset.__init__(self, root, transform=transform, **kwargs)
+        if self.bg is None:
+            self.bg = 'rnd'
+        if self.same_shapes is None:
+            self.same_shapes = False
+        self.angles = (1, 11)
+        self.rgb_diffs, self.rgb_probs = _normal_dist_ints(25, scale=5)
+        self.img_paths = sorted(glob.glob(self.imgdir + '*.png'))
+        self.colour_dist = colour_dist
+        if self.colour_dist is not None:
+            self.colour_dist = np.loadtxt(self.colour_dist, delimiter=',', dtype=int)
+
+    def _prepare_train_imgs(self, masks, others_colour, target_colour):
+        others_colour = np.array(others_colour).astype('float32') / 255
+        target_colour = np.array(target_colour).astype('float32') / 255
+        imgs = self._prepare_out_imgs(masks, others_colour, target_colour, _random_place)
         return imgs
 
     def _get_target_colour(self):
@@ -187,7 +203,7 @@ class ShapeTrain(torch_data.Dataset):
     def _get_others_colour(self, target_colour):
         others_colour = []
         # others_diff = np.random.choice(self.rgb_diffs, size=3, p=self.rgb_probs)
-        others_diff = [random.choice([1, -1]) * random.randint(5, 50) for _ in range(3)]
+        others_diff = [random.choice([1, -1]) * random.randint(1, 128) for _ in range(3)]
         for chn_ind in range(3):
             chn_colour = target_colour[chn_ind] + others_diff[chn_ind]
             if chn_colour < 0 or chn_colour > 255:
@@ -195,38 +211,39 @@ class ShapeTrain(torch_data.Dataset):
             others_colour.append(chn_colour)
         return others_colour
 
+    def _prepare_angle_paths(self, target_path, num_angles):
+        angle = int(ntpath.basename(target_path[:-4]).split('_')[-1].replace('angle', ''))
+        # angles pool
+        angles_pool = np.arange(*self.angles).tolist()
+        angles_pool.remove(angle)
+        random.shuffle(angles_pool)
+        angle_paths = [
+            target_path.replace('angle%d.png' % angle, 'angle%d.png' % angles_pool[i]) for i in
+            range(num_angles)
+        ]
+        return angle_paths
+
     def __len__(self):
         return len(self.img_paths)
 
 
-class ShapeVal(torch_data.Dataset):
+class ShapeVal(ShapeDataset):
 
     def __init__(self, root, transform=None, target_colour=None, others_colour=None, **kwargs):
-        self.root = root
-        self.transform = transform
-        self.target_size = 224
-        self.imgdir = '%s/shape2D/' % self.root
+        ShapeDataset.__init__(self, root, transform=transform, **kwargs)
+        if self.bg is None:
+            self.bg = 128
+        if self.same_shapes is None:
+            self.same_shapes = True
         stimuli_path = '%s/validation.cvs' % self.root
         self.stimuli = np.loadtxt(stimuli_path, delimiter=',', dtype=int)
         self.target_colour = target_colour
         self.others_colour = others_colour
 
     def _prepare_test_imgs(self, masks):
-        imgs = []
-        for mask_ind, mask in enumerate(masks):
-            mask = cv2.resize(mask, (128, 128), interpolation=cv2.INTER_NEAREST)
-            current_colour = self.target_colour if mask_ind == 0 else self.others_colour
-            current_colour = current_colour.squeeze()
-
-            mask_img = np.zeros((*mask.shape, 3), dtype='float32') + 0.5
-            for chn_ind in range(3):
-                current_chn = mask_img[:, :, chn_ind]
-                current_chn[mask == 255] = current_colour[chn_ind]
-
-            imgs.append(_random_place(mask_img, self.target_size, 0.5))
-
-        if self.transform is not None:
-            imgs = self.transform(imgs)
+        others_colour = self.others_colour.squeeze()
+        target_colour = self.target_colour.squeeze()
+        imgs = self._prepare_out_imgs(masks, others_colour, target_colour, _centre_place)
         return imgs
 
     def __len__(self):
@@ -241,25 +258,12 @@ class ShapeOddOneOutTrain(ShapeTrain):
 
     def __getitem__(self, item):
         target_path = self.img_paths[item]
-        angle = int(ntpath.basename(target_path[:-4]).split('_')[-1].replace('angle', ''))
+        if self.same_shapes:
+            other_paths = [target_path, target_path, target_path]
+        else:
+            other_paths = self._prepare_angle_paths(target_path, 3)
 
-        # angles pool
-        angles_pool = np.arange(*self.angles).tolist()
-        angles_pool.remove(angle)
-        random.shuffle(angles_pool)
-
-        other_paths = [
-            target_path.replace('angle%d.png' % angle, 'angle%d.png' % angles_pool[0]),
-            target_path.replace('angle%d.png' % angle, 'angle%d.png' % angles_pool[1]),
-            target_path.replace('angle%d.png' % angle, 'angle%d.png' % angles_pool[2]),
-        ]
-
-        masks = [
-            io.imread(target_path),
-            io.imread(other_paths[0]),
-            io.imread(other_paths[1]),
-            io.imread(other_paths[2])
-        ]
+        masks = [io.imread(target_path), *[io.imread(opath) for opath in other_paths]]
 
         # set the colours
         target_colour = self._get_target_colour()
@@ -281,13 +285,15 @@ class ShapeOddOneOutVal(ShapeVal):
         self.num_stimuli = 4
 
     def __getitem__(self, item):
-        masks = []
-        for i in range(4):
-            # image names start from 1
-            imgi = item + 1
-            masks.append(
-                io.imread('%s/img_shape%d_angle%d.png' % (self.imgdir, imgi, self.stimuli[item, i]))
-            )
+        # image names start from 1
+        imgi = item + 1
+        base_path = '%s/img_shape%d_angle' % (self.imgdir, imgi)
+        target_path = '%s%d.png' % (base_path, self.stimuli[item, 0])
+        if self.same_shapes:
+            other_paths = [target_path, target_path, target_path]
+        else:
+            other_paths = ['%s%d.png' % (base_path, self.stimuli[item, i]) for i in range(3)]
+        masks = [io.imread(target_path), *[io.imread(opath) for opath in other_paths]]
 
         imgs = self._prepare_test_imgs(masks)
 
@@ -307,27 +313,18 @@ class Shape2AFCTrain(ShapeTrain):
 
     def __getitem__(self, item):
         target_path = self.img_paths[item]
-        angle = int(ntpath.basename(target_path[:-4]).split('_')[-1].replace('angle', ''))
+        if self.same_shapes:
+            other_paths = target_path
+        else:
+            other_paths = self._prepare_angle_paths(target_path, 1)
 
-        # angles pool
-        angles_pool = np.arange(*self.angles).tolist()
-        angles_pool.remove(angle)
-        random.shuffle(angles_pool)
-
-        # other_paths = target_path.replace('angle%d.png' % angle, 'angle%d.png' % angles_pool[0])
-        # FIXME: should target path and other paths be the same?
-        other_paths = target_path
-
-        masks = [
-            io.imread(target_path),
-            io.imread(other_paths)
-        ]
+        masks = [io.imread(target_path), io.imread(other_paths)]
 
         # set the colours
         target_colour = self._get_target_colour()
         if random.random() < 0.5:
-            others_colour = target_colour
             target = 1
+            others_colour = target_colour
         else:
             target = 0
             others_colour = self._get_others_colour(target_colour)
@@ -343,13 +340,14 @@ class Shape2AFCVal(ShapeVal):
         ShapeVal.__init__(self, root, transform=transform, **kwargs)
 
     def __getitem__(self, item):
-        masks = []
-        for _ in range(2):
-            # image names start from 1
-            imgi = item + 1
-            masks.append(
-                io.imread('%s/img_shape%d_angle%d.png' % (self.imgdir, imgi, self.stimuli[item, 0]))
-            )
+        # image names start from 1
+        imgi = item + 1
+        target_path = '%s/img_shape%d_angle%d.png' % (self.imgdir, imgi, self.stimuli[item, 0])
+        if self.same_shapes:
+            other_paths = target_path
+        else:
+            other_paths = '%s/img_shape%d_angle%d.png' % (self.imgdir, imgi, self.stimuli[item, 1])
+        masks = [io.imread(target_path), io.imread(other_paths)]
 
         imgs = self._prepare_test_imgs(masks)
 
@@ -358,18 +356,18 @@ class Shape2AFCVal(ShapeVal):
         return imgs[0], imgs[1], target
 
 
-def _random_place(mask_img, target_size, bg):
-    # TODO: option for type of the background
-    img = np.zeros((target_size, target_size, 3), dtype='float32') + bg
+def _centre_place(mask_size, target_size):
+    srow = int((target_size[0] - mask_size[0]) / 2)
+    scol = int((target_size[1] - mask_size[1]) / 2)
 
-    mask_size = mask_img.shape
-    srow = int(mask_size[0] / 2)
-    erow = srow + mask_size[0]
-    scol = int(mask_size[1] / 2)
-    ecol = scol + mask_size[1]
-    img[srow:erow, scol:ecol] = mask_img
+    return srow, scol
 
-    return img
+
+def _random_place(mask_size, target_size):
+    srow = random.randint(0, target_size[0] - mask_size[0])
+    scol = random.randint(0, target_size[1] - mask_size[1])
+
+    return srow, scol
 
 
 def train_set(root, target_size, preprocess, task, **kwargs):
